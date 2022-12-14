8fa7805c5be5bc2b7114a703a58b1febc94ca6e8
Guillaume.GROLLERON <guillaume.grolleron@gmail.com>
2022-12-14 16:20:09 +0100

    -new code for the SPE fit

---
+++ nectarchain/calibration/NectarGain/SPEfit/NectarGainSPE_combined.py
@@ -0,0 +1,14 @@
+from ..utils.error import DifferentPixelsID
+
+from .NectarGainSPE_singlerun import NectarGainSPESingle
+
+
+class NectarGainSPESingleHHV(NectarGainSPESingle):
+    """class to perform fit of the 1400V signal and pedestal"""
+
+
+class NectarGainSPESingleCombined(NectarGainSPESingle):
+    """class to perform fit of the 1400V and 1000V signal and pedestal"""
+
+class NectarGainSPESingleCombinedNoPed(NectarGainSPESingle):
+    """class to perform fit of the 1400V and 1000V signal"""
---
+++ nectarchain/calibration/NectarGain/SPEfit/__init__.py
@@ -0,0 +1,3 @@
+#from . import NectarGAIN
+from .NectarGainSPE_singlerun import *
+from .parameters import *
