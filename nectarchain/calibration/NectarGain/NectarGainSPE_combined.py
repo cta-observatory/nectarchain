a40a3a9b0174358874adb0f5f92125062939cd48
Guillaume.GROLLERON <guillaume.grolleron@gmail.com>
2023-01-04 14:45:29 +0100

    clean

--- nectarchain/calibration/NectarGain/NectarGainSPE_combined.py
+++
@@ -1,9 +0,0 @@
-class NectarGainSPESingleHHV(NectarGainSPESingle):
-    """class to perform fit of the 1400V signal and pedestal"""
-
-
-class NectarGainSPESingleCombined(NectarGainSPESingle):
-    """class to perform fit of the 1400V and 1000V signal and pedestal"""
-
-class NectarGainSPESingleCombinedNoPed(NectarGainSPESingle):
-    """class to perform fit of the 1400V and 1000V signal"""
