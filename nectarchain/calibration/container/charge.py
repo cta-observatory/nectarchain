-bugfix : in histo low gain and high gain computation np.int16 -> np.uint16

nectarchain/calibration/container/charge.py
@@ -212,12 +212,12 @@ class ChargeContainer() :
 
     def histo_hg(self,n_bins : int = 1000,autoscale : bool = False) -> np.ndarray:
         if autoscale : 
-            all_range = np.arange(np.int16(np.min(self.charge_hg)) + 0.5,np.int16(np.max(self.charge_hg)) + 0.5,1)
-            hist_ma = ma.masked_array(np.zeros((self.charge_hg.shape[1],all_range.shape[0]),dtype = np.int16), mask=np.zeros((self.charge_hg.shape[1],all_range.shape[0]),dtype = bool))
+            all_range = np.arange(np.uint16(np.min(self.charge_hg)) + 0.5,np.uint16(np.max(self.charge_hg)) + 0.5,1)
+            hist_ma = ma.masked_array(np.zeros((self.charge_hg.shape[1],all_range.shape[0]),dtype = np.uint16), mask=np.zeros((self.charge_hg.shape[1],all_range.shape[0]),dtype = bool))
             charge_ma = ma.masked_array(np.zeros((self.charge_hg.shape[1],all_range.shape[0])), mask=np.zeros((self.charge_hg.shape[1],all_range.shape[0]),dtype = bool))
             
             for i in range(self.charge_hg.shape[1]) :
-                hist,charge = np.histogram(self.charge_hg.T[i],bins=np.arange(np.int16(np.min(self.charge_hg.T[i])),np.int16(np.max(self.charge_hg.T[i])) + 1,1))
+                hist,charge = np.histogram(self.charge_hg.T[i],bins=np.arange(np.uint16(np.min(self.charge_hg.T[i])),np.uint16(np.max(self.charge_hg.T[i])) + 1,1))
                 charge_edges = np.array([np.mean(charge[i:i+2],axis = 0) for i in range(charge.shape[0]-1)]) 
                 mask = (all_range >= charge_edges[0]) * (all_range <= charge_edges[-1])
 
@@ -241,12 +241,12 @@ class ChargeContainer() :
 
     def histo_lg(self,n_bins: int = 1000,autoscale : bool = False) -> np.ndarray:
         if autoscale : 
-            all_range = np.arange(np.int16(np.min(self.charge_lg)) + 0.5,np.int16(np.max(self.charge_lg)) + 0.5,1)
-            hist_ma = ma.masked_array(np.zeros((self.charge_lg.shape[1],all_range.shape[0]),dtype = np.int16), mask=np.zeros((self.charge_lg.shape[1],all_range.shape[0]),dtype = bool))
+            all_range = np.arange(np.uint16(np.min(self.charge_lg)) + 0.5,np.uint16(np.max(self.charge_lg)) + 0.5,1)
+            hist_ma = ma.masked_array(np.zeros((self.charge_lg.shape[1],all_range.shape[0]),dtype = np.uint16), mask=np.zeros((self.charge_lg.shape[1],all_range.shape[0]),dtype = bool))
             charge_ma = ma.masked_array(np.zeros((self.charge_lg.shape[1],all_range.shape[0])), mask=np.zeros((self.charge_lg.shape[1],all_range.shape[0]),dtype = bool))
             
             for i in range(self.charge_lg.shape[1]) :
-                hist,charge = np.histogram(self.charge_lg.T[i],bins=np.arange(np.int16(np.min(self.charge_lg.T[i])),np.int16(np.max(self.charge_lg.T[i])) + 1,1))
+                hist,charge = np.histogram(self.charge_lg.T[i],bins=np.arange(np.uint16(np.min(self.charge_lg.T[i])),np.uint16(np.max(self.charge_lg.T[i])) + 1,1))
                 charge_edges = np.array([np.mean(charge[i:i+2],axis = 0) for i in range(charge.shape[0]-1)]) 
                 mask = (all_range >= charge_edges[0]) * (all_range <= charge_edges[-1])
 