6298f09f70f9e5ffa30c31b87eb15072190f3c9c
sona-patel <sonalpatel.982@gmail.com>
2023-02-14 11:54:13 +0100

    flake8 to src/nectarchain/dqm/dqm_summary_processor.py

--- src/nectarchain/dqm/dqm_summary_processor.py
+++ src/nectarchain/dqm/dqm_summary_processor.py
@@ -1,33 +1,5 @@
-
 import pickle
 
-import os
-import sys
-
-
-#from ctapipe.io import event_source
-import sys
- 
-from matplotlib import pyplot as plt
-import numpy as np
-from scipy.stats import norm
-from traitlets.config.loader import Config 
-from astropy.io import fits
-
-# ctapipe modules
-from ctapipe import utils
-from ctapipe.visualization import CameraDisplay
-#from ctapipe.plotting.camera import CameraPlotter
-from ctapipe.image.extractor import *
-from ctapipe.io import EventSeeker 
-from ctapipe.instrument import CameraGeometry
-
-from ctapipe.io.hdf5tableio import HDF5TableWriter, HDF5TableReader
-
-from ctapipe.io import EventSource
-import ctapipe.instrument.camera.readout
-
-from astropy import time as astropytime
 
 class dqm_summary:
     def __init__(self):
@@ -36,7 +8,7 @@ class dqm_summary:
     def DefineForRun(self, reader1):
         for i, evt1 in enumerate(reader1):
             self.FirstReader = reader1
-            self.Samp= len(evt1.r0.tel[0].waveform[0][0])
+            self.Samp = len(evt1.r0.tel[0].waveform[0][0])
             self.Chan = len(evt1.r0.tel[0].waveform[0])
 
         return self.Chan, self.Samp
@@ -53,13 +25,12 @@ class dqm_summary:
     def GetResults(self):
         print('Processor 4')
 
-    def PlotResults(self, name,FigPath,k, M, M_ped, Mean_M_overChan, Mean_M_ped_overChan):
+    def PlotResults(self, name, FigPath, k, M, M_ped, Mean_M_overChan,
+                    Mean_M_ped_overChan):
         print('Processor 5')
 
-
-    def WriteAllResults(self,path, DICT):
+    def WriteAllResults(self, path, DICT):
         PickleName = path + '_Results.pickle'
         with open(PickleName, 'wb') as handle:
-            pickle.dump(DICT, handle, protocol=pickle.HIGHEST_PROTOCOL) 
+            pickle.dump(DICT, handle, protocol=pickle.HIGHEST_PROTOCOL)
         return None
-
