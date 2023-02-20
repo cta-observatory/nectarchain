bugfix : -photostat : use only fullwaveform sum for extract charge from Ped  and FF  -figpath missing in SPE fit computation

src/nectarchain/calibration/NectarGain/SPEfit/NectarGainSPE_singlerun.py
@@ -177,7 +177,7 @@ class NectarGainSPESingle(NectarGainSPE):
                 return output
             return task
 
-        def task_bis(i,funct,parameters : Parameters,pixels_id : int,charge : np.ndarray, histo : np.ndarray,_class : str,**kwargs) : 
+        def task_bis(i,funct,parameters : Parameters,pixels_id : int,charge : np.ndarray, histo : np.ndarray,_class : str,kwargs) : 
             log.info(f"i = {i}")
             log.debug(f"{kwargs}")
             if charge.mask.all() : 
@@ -218,7 +218,7 @@ class NectarGainSPESingle(NectarGainSPE):
                     for logger in loggers : logger.setLevel(logging.FATAL)
 
                     result = pool.starmap_async(task_bis, 
-                    [(i,self.Chi2_static(i),copy.deepcopy(self.parameters),self.__pixels_id[i], self.__charge[i], self.__histo[i], self.__class__) for i in tqdm(range(self.npixels))],
+                    [(i,self.Chi2_static(i),copy.deepcopy(self.parameters),self.__pixels_id[i], self.__charge[i], self.__histo[i], self.__class__,kwargs) for i in tqdm(range(self.npixels))],
                     chunksize = chunksize,
                     error_callback=Multiprocessing.custom_error_callback
                     )
@@ -275,7 +275,7 @@ class NectarGainSPESingle(NectarGainSPE):
                     for logger in loggers : logger.setLevel(logging.FATAL)
 
                     result = pool.starmap_async(task_bis, 
-                    [(i,self.Chi2_static(i),copy.deepcopy(self.parameters),self.__pixels_id[i], self.__charge[i], self.__histo[i], self.__class__) for i in tqdm(pixels)],
+                    [(i,self.Chi2_static(i),copy.deepcopy(self.parameters),self.__pixels_id[i], self.__charge[i], self.__histo[i], self.__class__,kwargs) for i in tqdm(pixels)],
                     chunksize = chunksize,
                     error_callback=Multiprocessing.custom_error_callback
                     )

src/nectarchain/user_scripts/ggrolleron/gain_PhotoStat_computation.py
@@ -72,7 +72,7 @@ def main(args) :
     figpath = os.environ.get('NECTARCHAIN_FIGURES')
 
 
-    photoStat_FFandPed = PhotoStatGainFFandPed(args.FF_run_number, args.ped_run_number, SPEresults = args.SPE_fit_results)    
+    photoStat_FFandPed = PhotoStatGainFFandPed(args.FF_run_number, args.ped_run_number, SPEresults = args.SPE_fit_results,method = args.chargeExtractorPath)    
     photoStat_FFandPed.run()
     photoStat_FFandPed.save(f"{os.environ.get('NECTARCAMDATA')}/../PhotoStat/data/PhotoStat-FF{args.FF_run_number}-ped{args.ped_run_number}-SPEres{args.SPE_fit_results_tag}-{args.chargeExtractorPath}/",overwrite = args.overwrite)
     log.info(f"BF^2 HG : {np.power(np.mean(photoStat_FFandPed.BHG),2)}")