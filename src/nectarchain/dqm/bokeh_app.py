1aa5518a7112b5c079988dc304e907706dce991e
Jean-Philippe Lenain <jlenain@in2p3.fr>
2023-06-05 20:10:36 +0000

    Work on Bokeh app

--- src/nectarchain/dqm/bokeh_app.py
+++ src/nectarchain/dqm/bokeh_app.py
@@ -3,7 +3,8 @@ import numpy as np
 from markupsafe import Markup
 
 # bokeh imports
-from bokeh.layouts import column, row
+from bokeh.layouts import layout, column, row
+from bokeh.io import show
 from bokeh.models import ColumnDataSource, Select  # , NumericInput
 from bokeh.plotting import figure, curdoc
 
@@ -13,24 +14,39 @@ from ctapipe.instrument import CameraGeometry
 
 from db_utils import SaveDB
 
-db = SaveDB()
-DATALIST = list(db.root.keys())
+def get_rundata(src, runnb):
+    run_data = src[runnb.value]
+    return run_data
+
+def make_camera_display(source):
+    # Example camera display
+    geom = CameraGeometry.from_name("NectarCam-003")
+    image = source['Results_MeanCameraDisplay_HighGain']['CAMERA-AVERAGE-PHY-OverEVENTS-OverSamp-HIGH-GAIN']
+    display = CameraDisplay(geometry=geom, image=image)
+    return display
+    
+def update_camera_display(attr, old, new):
+    runnb = run_select.value
+    
+    src = get_rundata(db, runnb)
+    source.data.update(src.data)
+    
+    
+
+db = SaveDB().root
+runnb = sorted(list(db.keys()))[-1]
+print(sorted(list(db.keys())))
 
 # runnb_input = NumericInput(value=db.root.keys()[-1], title="NectarCAM run number")
-runnb_input = Select(value = 'Choose the run', title = 'Choose the run', options = DATALIST)
+run_select = Select(value=runnb, title='Run', options=sorted(list(db.keys())))
 
-# Example camera display
-geom = CameraGeometry.from_name("NectarCam-003")
-image = db.root[runnb_input]['Results_MeanCameraDisplay_HighGain']['CAMERA-AVERAGE-PHY-OverEVENTS-OverSamp-HIGH-GAIN']
-display = CameraDisplay(geom, image)
+source = get_rundata(db, run_select)
+plot = make_camera_display(source)
 
+run_select.on_change('value', update_camera_display)
 
-def callback(attr, new, old):
-    # TODO to be implemented
-    print(attr, new, old)
-
-
-display.enable_pixel_picker(callback)
+controls = row(run_select)
 
 doc = curdoc()
-doc.title('NectarCAM DQM web app')
+doc.add_root(layout(controls, plot))
+doc.title('NectarCAM Data Quality Monitoring web app')
