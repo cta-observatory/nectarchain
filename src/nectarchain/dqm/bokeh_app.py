bf8d050bb1e6e8af1f5df3d5108e1a2b25963ecf
Jean-Philippe Lenain <jlenain@in2p3.fr>
2023-06-05 21:40:37 +0000

    Working on Bokeh app

--- src/nectarchain/dqm/bokeh_app.py
+++ src/nectarchain/dqm/bokeh_app.py
@@ -3,10 +3,10 @@ import numpy as np
 from markupsafe import Markup
 
 # bokeh imports
-from bokeh.layouts import layout, column, row
+from bokeh.layouts import layout, row, gridplot
 from bokeh.io import show
-from bokeh.models import ColumnDataSource, Select  # , NumericInput
-from bokeh.plotting import figure, curdoc
+from bokeh.models import Select  # , NumericInput
+from bokeh.plotting import curdoc
 
 # ctapipe imports
 from ctapipe.visualization.bokeh import CameraDisplay
@@ -14,16 +14,20 @@ from ctapipe.instrument import CameraGeometry
 
 from db_utils import SaveDB
 
+NOTDISPLAY = ['Results_TriggerStatistics', 'Results_MeanWaveForms_HighGain', 'Results_MeanWaveForms_LowGain']
+
+geom = CameraGeometry.from_name("NectarCam-003")
+
 def get_rundata(src, runnb):
     run_data = src[runnb.value]
     return run_data
 
-def make_camera_display(source):
+def make_camera_display(source, parent_key, child_key):
     # Example camera display
-    geom = CameraGeometry.from_name("NectarCam-003")
-    image = source['Results_MeanCameraDisplay_HighGain']['CAMERA-AVERAGE-PHY-OverEVENTS-OverSamp-HIGH-GAIN']
+    image = source[parent_key][child_key]
     display = CameraDisplay(geometry=geom, image=image)
     display.add_colorbar()
+    display.figure.title = child_key
     return display.figure
     
 def update_camera_display(attr, old, new):
@@ -31,23 +35,30 @@ def update_camera_display(attr, old, new):
     
     src = get_rundata(db, runnb)
     source.data.update(src.data)
-    
-    
 
 db = SaveDB().root
 runnb = sorted(list(db.keys()))[-1]
 print(sorted(list(db.keys())))
 
 # runnb_input = NumericInput(value=db.root.keys()[-1], title="NectarCAM run number")
-run_select = Select(value=runnb, title='Run', options=sorted(list(db.keys())))
+run_select = Select(value=runnb, title='NectarCAM run number', options=sorted(list(db.keys())))
 
 source = get_rundata(db, run_select)
-plot = make_camera_display(source)
+plots = []
+for parentkey in db[runnb].keys():
+    if parentkey not in NOTDISPLAY:
+        for childkey in db[runnb][parentkey].keys():
+            print(f'Preparing plot for {parentkey}, {childkey}')
+            try:
+                plots.append(make_camera_display(source,
+                                                 parent_key=parentkey,
+                                                 child_key=childkey))
+            except BokehUserWarning:
+                pass
 
 run_select.on_change('value', update_camera_display)
 
 controls = row(run_select)
 
-doc = curdoc()
-doc.add_root(layout(controls, plot))
-# doc.title('NectarCAM Data Quality Monitoring web app')
+curdoc().add_root(layout(controls, [plots[x:x+6] for x in range(0, len(plots), 6)]))
+curdoc().title = 'NectarCAM Data Quality Monitoring web app'
