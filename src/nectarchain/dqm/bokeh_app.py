bcfd513c53b2ceb019a0827eaf02d8f80e827103
Jean-Philippe Lenain <jlenain@in2p3.fr>
2023-06-06 15:01:18 +0000

    Working on updated data in Bokeh app

--- src/nectarchain/dqm/bokeh_app.py
+++ src/nectarchain/dqm/bokeh_app.py
@@ -1,3 +1,4 @@
+import collections
 import numpy as np
 
 # bokeh imports
@@ -18,58 +19,74 @@ geom = CameraGeometry.from_name("NectarCam-003")
 geom = geom.transform_to(EngineeringCameraFrame())
 
 def get_rundata(src, runid):
-    run_data = src[runid.value]
+    run_data = src[runid]
     return run_data
 
 def make_camera_displays(db, source, runid):
-    plots = []
+    displays = collections.defaultdict(dict)
     for parentkey in db[runid].keys():
         if parentkey not in NOTINDISPLAY:
             for childkey in db[runid][parentkey].keys():
-                print(f'Preparing plot for {parentkey}, {childkey}')
-                # try:
-                plots.append(make_camera_display(source,
-                                                parent_key=parentkey,
-                                                child_key=childkey))
-                # except BokehUserWarning:
-                #     pass
-    return plots
+                print(f'Run id {runid} Preparing plot for {parentkey}, {childkey}')
+                displays[parentkey][childkey] = make_camera_display(source,
+                                                                    parent_key=parentkey,
+                                                                    child_key=childkey)
+    return dict(displays)
 
 def make_camera_display(source, parent_key, child_key):
     # Example camera display
     image = source[parent_key][child_key]
-    display = CameraDisplay(geometry=geom, image=image)
+    display = CameraDisplay(geometry=geom)
+    try:
+        display.image = image
+    except ValueError:
+        image = np.zeros(shape=display.image.shape)
+        display.image = image
     display.add_colorbar()
     display.figure.title = child_key
-    return display.figure
-
+    return display
+   
 def update_camera_displays(attr, old, new):
     runid = run_select.value
+    new_rundata = get_rundata(db, runid)
+    
     for parentkey in db[runid].keys():
         if parentkey not in NOTINDISPLAY:
             for childkey in db[runid][parentkey].keys():
-                update_camera_display()
-    
-def update_camera_display(attr, old, new):
-    runid = run_select.value
-    
-    src = get_rundata(db, runid)
-    source.data.update(src.data)
+                print(f'Run id {runid} Updating plot for {parentkey}, {childkey}')
+                # try:
+                image = new_rundata[parentkey][childkey]
+                displays[parentkey][childkey].image = image
+                # TODO: TRY TO USE `stream`` INSTEAD, ON UPDATES:
+                # display.datasource.stream(new_data)
+                # displays[parentkey][childkey].datasource.stream(image)
+
 
 db = DQMDB().root
-runid = sorted(list(db.keys()))[-1]
-print(sorted(list(db.keys())))
+runids = sorted(list(db.keys()))
+runid = runids[0]
 
 # runid_input = NumericInput(value=db.root.keys()[-1], title="NectarCAM run number")
-run_select = Select(value=runid, title='NectarCAM run number', options=sorted(list(db.keys())))
+run_select = Select(value=runid, title='NectarCAM run number', options=runids)
 
-source = get_rundata(db, run_select)
-plots = make_camera_displays(db, source, runid)
+source = get_rundata(db, run_select.value)
+displays = make_camera_displays(db, source, runid)
 
 run_select.on_change('value', update_camera_displays)
 
 controls = row(run_select)
 
+# # TEST:
+# attr = 'value'
+# old = runid
+# new = runids[1]
+# update_camera_displays(attr, old, new)
+
 ncols = 3
-curdoc().add_root(layout(controls, [plots[x:x+ncols-1] for x in range(0, len(plots), ncols-1)]))
+plots = [displays[parentkey][childkey].figure for parentkey in displays.keys() for childkey in displays[parentkey].keys()]
+curdoc().add_root(layout([[controls],
+                          [[plots[x:x+ncols] for x in range(0, len(plots), ncols)]]],
+                         sizing_mode='scale_width'
+                         )
+                  )
 curdoc().title = 'NectarCAM Data Quality Monitoring web app'
