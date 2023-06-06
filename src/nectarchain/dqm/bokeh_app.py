a0ed7e99b1e0bad0264b1b02ae5198f6b2d775bc
Jean-Philippe Lenain <jlenain@in2p3.fr>
2023-06-06 09:15:50 +0000

    Working on Bokeh app

--- src/nectarchain/dqm/bokeh_app.py
+++ src/nectarchain/dqm/bokeh_app.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from markupsafe import Markup
+# from markupsafe import Markup
 
 # bokeh imports
 from bokeh.layouts import layout, row, gridplot
@@ -11,16 +11,32 @@ from bokeh.plotting import curdoc
 # ctapipe imports
 from ctapipe.visualization.bokeh import CameraDisplay
 from ctapipe.instrument import CameraGeometry
+from ctapipe.coordinates import EngineeringCameraFrame
 
 from db_utils import SaveDB
 
 NOTDISPLAY = ['Results_TriggerStatistics', 'Results_MeanWaveForms_HighGain', 'Results_MeanWaveForms_LowGain']
 
 geom = CameraGeometry.from_name("NectarCam-003")
+geom = geom.transform_to(EngineeringCameraFrame())
 
-def get_rundata(src, runnb):
-    run_data = src[runnb.value]
+def get_rundata(src, runid):
+    run_data = src[runid.value]
     return run_data
+
+def make_camera_displays(db, source, runid):
+    plots = []
+    for parentkey in db[runid].keys():
+        if parentkey not in NOTDISPLAY:
+            for childkey in db[runid][parentkey].keys():
+                print(f'Preparing plot for {parentkey}, {childkey}')
+                # try:
+                plots.append(make_camera_display(source,
+                                                parent_key=parentkey,
+                                                child_key=childkey))
+                # except BokehUserWarning:
+                #     pass
+    return plots
 
 def make_camera_display(source, parent_key, child_key):
     # Example camera display
@@ -29,36 +45,33 @@ def make_camera_display(source, parent_key, child_key):
     display.add_colorbar()
     display.figure.title = child_key
     return display.figure
+
+def update_camera_displays(db, runid):
+    for parentkey in db[runid].keys():
+        if parentkey not in NOTDISPLAY:
+            for childkey in db[runid][parentkey].keys():
+                update_camera_display()
     
 def update_camera_display(attr, old, new):
-    runnb = run_select.value
+    runid = run_select.value
     
-    src = get_rundata(db, runnb)
+    src = get_rundata(db, runid)
     source.data.update(src.data)
 
 db = SaveDB().root
-runnb = sorted(list(db.keys()))[-1]
+runid = sorted(list(db.keys()))[-1]
 print(sorted(list(db.keys())))
 
-# runnb_input = NumericInput(value=db.root.keys()[-1], title="NectarCAM run number")
-run_select = Select(value=runnb, title='NectarCAM run number', options=sorted(list(db.keys())))
+# runid_input = NumericInput(value=db.root.keys()[-1], title="NectarCAM run number")
+run_select = Select(value=runid, title='NectarCAM run number', options=sorted(list(db.keys())))
 
 source = get_rundata(db, run_select)
-plots = []
-for parentkey in db[runnb].keys():
-    if parentkey not in NOTDISPLAY:
-        for childkey in db[runnb][parentkey].keys():
-            print(f'Preparing plot for {parentkey}, {childkey}')
-            try:
-                plots.append(make_camera_display(source,
-                                                 parent_key=parentkey,
-                                                 child_key=childkey))
-            except BokehUserWarning:
-                pass
+plots = make_camera_displays(db, source, runid)
 
-run_select.on_change('value', update_camera_display)
+run_select.on_change('value', update_camera_displays)
 
 controls = row(run_select)
 
-curdoc().add_root(layout(controls, [plots[x:x+6] for x in range(0, len(plots), 6)]))
+ncols = 3
+curdoc().add_root(layout(controls, [plots[x:x+ncols-1] for x in range(0, len(plots), ncols-1)]))
 curdoc().title = 'NectarCAM Data Quality Monitoring web app'
