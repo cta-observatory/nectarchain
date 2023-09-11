41c6a9269ea16ae46f082adfe3df16a0ca5def9b
guillaume.grolleron <ggroller@lpnhe.in2p3.fr>
2023-09-11 23:11:16 +0200

    add low gain in contructor (only high gain before)

--- src/nectarchain/calibration/makers/gain/gainMakers.py
+++ src/nectarchain/calibration/makers/gain/gainMakers.py
@@ -12,7 +12,7 @@ import astropy.units as u
 
 from ..core import CalibrationMaker
 
-__all__ = ["GainMaker"]
+__all__ = ["high_gainMaker"]
 
 class GainMaker(CalibrationMaker) : 
     """mother class for of the gain calibration
@@ -21,18 +21,27 @@ class GainMaker(CalibrationMaker) :
 #constructors
     def __init__(self,*args,**kwargs) -> None:
         super().__init__(*args,**kwargs)
-        self.__gain = np.empty((self.npixels),dtype = np.float64)
-        self._results.add_column(Column(data = self.__gain,name = "gain",unit = u.dimensionless_unscaled))
-        self._results.add_column(Column(np.empty((self.npixels,2),dtype = np.float64),"gain_error",unit = u.dimensionless_unscaled))
+        self.__high_gain = np.empty((self.npixels),dtype = np.float64)
+        self.__low_gain = np.empty((self.npixels),dtype = np.float64)
+        self._results.add_column(Column(data = self.__high_gain,name = "high_gain",unit = u.dimensionless_unscaled))
+        self._results.add_column(Column(np.empty((self.npixels,2),dtype = np.float64),"high_gain_error",unit = u.dimensionless_unscaled))
+        self._results.add_column(Column(data = self.__low_gain,name = "low_gain",unit = u.dimensionless_unscaled))
+        self._results.add_column(Column(np.empty((self.npixels,2),dtype = np.float64),"low_gain_error",unit = u.dimensionless_unscaled))
         
         self._results.add_column(Column(np.zeros((self.npixels),dtype = bool),"is_valid",unit = u.dimensionless_unscaled))
 
 
 #getters and setters
     @property
-    def _gain(self) : return self.__gain
-    @_gain.setter
-    def _gain(self,value) : self.__gain = value
+    def _high_gain(self) : return self.__high_gain
+    @_high_gain.setter
+    def _high_gain(self,value) : self.__high_gain = value
     @property
-    def gain(self) : return copy(self.__gain)
+    def high_gain(self) : return copy(self.__high_gain)
+    @property
+    def _low_gain(self) : return self.__low_gain
+    @_low_gain.setter
+    def _low_gain(self,value) : self.__low_gain = value
+    @property
+    def low_gain(self) : return copy(self.__low_gain)
     
