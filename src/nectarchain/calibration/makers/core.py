177e577605c702f3485a4b874f20b04284ece24d
guillaume.grolleron <ggroller@lpnhe.in2p3.fr>
2023-09-11 23:10:32 +0200

    put I/O method in mother class

--- src/nectarchain/calibration/makers/core.py
+++ src/nectarchain/calibration/makers/core.py
@@ -3,6 +3,8 @@ import logging
 logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s %(message)s')
 log = logging.getLogger(__name__)
 log.handlers = logging.getLogger('__main__').handlers
+import os
+from pathlib import Path
 
 from abc import ABC, ABCMeta, abstractmethod
 
@@ -17,14 +19,15 @@ __all__ = [""]
 class CalibrationMaker(ABC) : 
     """mother class for all the calibration makers that can be defined to compute calibration coeficients from data
     """
+    _reduced_name = "Calibration"
 
 #constructors
     def __new__(cls,*args,**kwargs) : 
         return super(CalibrationMaker,cls).__new__(cls)
 
-    def __init__(self,*args,**kwargs) -> None:
+    def __init__(self,pixels_id,*args,**kwargs) -> None:
         super().__init__()
-        self.__pixels_id = kwargs.get("pixels_id", None)
+        self.__pixels_id = pixels_id
         self.__results = QTable()
         self.__results.add_column(Column(self.__pixels_id,"pixels_id",unit = u.dimensionless_unscaled))
         self.__results.meta['npixels'] = self.npixels
@@ -34,6 +37,13 @@ class CalibrationMaker(ABC) :
     @abstractmethod
     def make(self,*args,**kwargs) :
         pass
+
+#I/O method
+    def save(self,path,**kwargs) : 
+        path = Path(path)
+        os.makedirs(path,exist_ok = True)
+        log.info(f'data saved in {path}')
+        self._results.write(f"{path}/results_{self._reduced_name}.ecsv", format='ascii.ecsv',overwrite = kwargs.get("overwrite",False))
 
 #getters and setters
     @property
