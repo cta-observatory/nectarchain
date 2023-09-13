72da7ad32b56b877516bcdf7e9cc6eb6c4a9fac5
guillaume.grolleron <ggroller@lpnhe.in2p3.fr>
2023-09-13 03:03:16 +0200

    test extractor utlis

--- src/nectarchain/makers/extractor/utils.py
+++ src/nectarchain/makers/extractor/utils.py
@@ -6,5 +6,18 @@ log.handlers = logging.getLogger('__main__').handlers
 from ctapipe.containers import DL1CameraContainer 
 
 class CtapipeExtractor():
-    def get_image_peak_time(cameraContainer : DL1CameraContainer) : 
+    """
+    A class to extract the image and peak time from a DL1CameraContainer object.
+    """
+
+    def get_image_peak_time(cameraContainer):
+        """
+        Extracts the image and peak time from a DL1CameraContainer object.
+
+        Parameters:
+        cameraContainer (DL1CameraContainer): The DL1CameraContainer object to extract the image and peak time from.
+
+        Returns:
+        tuple: A tuple containing the image and peak time values from the container.
+        """
         return cameraContainer.image, cameraContainer.peak_time
