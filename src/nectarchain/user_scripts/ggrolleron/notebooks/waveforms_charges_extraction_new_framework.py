ecaa250c34cdabbd99f33d1404f47f926f6f0fdd
guillaume.grolleron <ggroller@lpnhe.in2p3.fr>
2023-10-23 14:42:49 +0200

    -preliminary tuto script to extract charges or waveforms using Tool and Component classes

---
+++ src/nectarchain/user_scripts/ggrolleron/notebooks/waveforms_charges_extraction_new_framework.py
@@ -0,0 +1,96 @@
+# %%
+from nectarchain.makers import ChargesNectarCAMCalibrationTool,WaveformsNectarCAMCalibrationTool
+from nectarchain.makers.component import get_valid_component
+from nectarchain.data.container import ChargesContainers,ChargesContainer,WaveformsContainer,WaveformsContainers
+from ctapipe.io import HDF5TableReader
+from ctapipe.containers import EventType
+
+
+# %%
+get_valid_component()
+
+# %%
+run_number = 3942
+
+# %%
+tool = WaveformsNectarCAMCalibrationTool(progress_bar = True,run_number = run_number,max_events = 500,log_level = 10)
+tool
+
+# %%
+tool = ChargesNectarCAMCalibrationTool(progress_bar = True,
+                                       method = 'LocalPeakWindowSum',
+                                       extractor_kwargs = {"window_width": 16, "window_shift": 4},
+                                       run_number = run_number,
+                                       max_events = 5000,
+                                       log_level = 10
+                                       )
+tool
+
+# %%
+tool.initialize()
+
+# %%
+tool.setup()
+
+# %%
+tool.start()
+
+# %%
+tool.finish()
+
+# %%
+container = WaveformsContainers()
+trigger_type = EventType.__members__
+
+
+with HDF5TableReader(f"/tmp/EventsLoopNectarCAMCalibrationTool_{run_number}.h5") as reader : 
+    for key,trigger in trigger_type.items() : 
+        try : 
+            tableReader = reader.read(table_name = f"/data/WaveformsContainer_0/{trigger.name}", containers = WaveformsContainer)
+            container.containers[trigger] = next(tableReader)
+        except Exception as err:
+            print(err)
+
+# %%
+container.containers
+
+# %%
+container = ChargesContainers()
+trigger_type = EventType.__members__
+
+
+with HDF5TableReader(f"/tmp/EventsLoopNectarCAMCalibrationTool_{run_number}.h5") as reader : 
+    for key,trigger in trigger_type.items() : 
+        try : 
+            tableReader = reader.read(table_name = f"/data/ChargesContainer_0/{trigger.name}", containers = ChargesContainer)
+            container.containers[trigger] = next(tableReader)
+        except Exception as err:
+            print(err)
+
+# %%
+container.containers
+
+# %%
+from nectarchain.makers.component import ChargesComponent
+import matplotlib.pyplot as plt
+import numpy as np
+
+# %%
+counts,charge = ChargesComponent.histo_hg(container.containers[EventType.FLATFIELD])
+charge.shape,counts.shape
+
+# %%
+fig,ax = plt.subplots(1,1,figsize = (7,7))
+ax.errorbar(
+    charge[30], 
+    counts[30], 
+    np.sqrt(counts[30]), 
+    zorder=0, 
+    fmt=".", 
+    label="data"
+    )
+
+# %%
+
+
+
