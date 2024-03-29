b306f303b0649c22d0a6472c9a830c63f2ff127d
L. Tibaldo <luigi.tibaldo@irap.omp.eu>
2024-03-29 23:21:26 +0100

    Add container/component/tool for pedestal estimation (#115)
    * create structure for pedestal component/tool
    
    * debugging tool structure
    
    * fix computation of mean, etc
    
    * Added container for pedestals
    
    * Minimal working tool/component
    
    * implemented time selection
    
    * Implemented filtering based on waveforms standard deviation
    
    * Add possibility to filter based on charge distribution
    
    * Restructured to use events_per_slice to reduce memory load
    
    * fully working version based on events_per_slice mechanism
    
    * completed doc strings
    
    * added tests for pedestal container
    
    * First go at unit test for pedestal tool
    
    * Unit tests for pedestal maker
    
    * finished unit test for pedestal tool
    
    * use test file on CC IN2P3 server
    
    ---------
    
    Co-authored-by: Luigi Tibaldo <tibaldo@users.noreply.github.com>

---
+++ src/nectarchain/user_scripts/ltibaldo/example_pedestal.py
@@ -0,0 +1,36 @@
+import logging
+import os
+import pathlib
+
+logging.basicConfig(
+    format="%(asctime)s %(name)s %(levelname)s %(message)s", level=logging.INFO
+)
+log = logging.getLogger(__name__)
+log.handlers = logging.getLogger("__main__").handlers
+
+from nectarchain.makers.calibration import (
+    PedestalNectarCAMCalibrationTool,
+)
+
+run_number = 3938
+max_events= 9999
+events_per_slice = 1000
+outfile = "/Users/ltibaldo/tmp/test_pedestal/pedestal_{}.h5".format(run_number)
+
+tool = PedestalNectarCAMCalibrationTool(
+    progress_bar=True,
+    run_number=run_number,
+    max_events=max_events,
+    events_per_slice=events_per_slice,
+    log_level=20,
+    output_path=outfile,
+    overwrite = True,
+    filter_method = 'WaveformsStdFilter',
+    wfs_std_threshold = 4.,
+)
+
+tool.initialize()
+tool.setup()
+
+tool.start()
+output = tool.finish(return_output_component=True)
