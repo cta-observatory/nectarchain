db10e7bbf2e96fe1c0ba38535fe47e8244bfdda1
Jean-Philippe Lenain <jlenain@in2p3.fr>
2024-07-03 16:42:53 +0200

    Revert "Revert "Try doc with sphinx-autoapi""
    This reverts commit 7f9a0b49107fa6e7600f49f80f84f50f2703bb98.

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import datetime
-import os
 import sys
 from pathlib import Path
 
@@ -16,8 +15,6 @@ if sys.version_info < (3, 11):
     import tomli as tomllib
 else:
     import tomllib
-
-sys.path.insert(0, os.path.abspath("../src"))
 
 pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
 pyproject = tomllib.loads(pyproject_path.read_text())
@@ -47,17 +44,14 @@ release = version
 extensions = [
     "sphinx.ext.githubpages",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
+    "autoapi.extension",
     "sphinx.ext.viewcode",
-    "sphinx_automodapi.automodapi",
-    "sphinx_automodapi.smart_resolver",
     "numpydoc",
 ]
 
+autoapi_dirs = ["../src"]
 numpydoc_show_class_members = False
 numpydoc_class_members_toctree = False
-autosummary_generate = True
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
