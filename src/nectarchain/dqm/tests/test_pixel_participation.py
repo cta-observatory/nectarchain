e521e75e9a3f99193d3462b36d63845baa3dd463
Halim Ashkar <halim.ashkar@cea.fr>
2024-07-19 14:48:20 +0200

    adding more CI tests to the pixel_particiapation module

--- src/nectarchain/dqm/tests/test_pixel_participation.py
+++ src/nectarchain/dqm/tests/test_pixel_participation.py
@@ -1,6 +1,8 @@
+import numpy as np
 from ctapipe.io import EventSource
 from ctapipe.utils import get_dataset_path
 from ctapipe_io_nectarcam.constants import HIGH_GAIN
+from tqdm import tqdm
 from traitlets.config import Config
 
 from nectarchain.dqm.pixel_participation import PixelParticipationHighLowGain
@@ -32,6 +34,18 @@ class TestPixelParticipation:
         reader1 = EventSource(input_url=path, config=config, max_events=1)
 
         Pix, Samp = PixelParticipationHighLowGain(HIGH_GAIN).DefineForRun(reader1)
-        print(Pix, Samp)
-        # self.assertEqual(Pix + Samp, 1915)
+        print("Pix, Samp", Pix, Samp)
+
+        PixelParticipationHighLowGain(HIGH_GAIN).ConfigureForRun(
+            path, Pix, Samp, reader1
+        )
+        print("Pix, Samp", Pix, Samp)
+
+        for evt in tqdm(reader1, total=1):
+            print("sum:", np.sum(evt.nectarcam.tel[0].svc.pixel_ids))
+            # PixelParticipationHighLowGain(HIGH_GAIN).ProcessEvent(evt, noped = False)
+
+        PixelParticipationHighLowGain(HIGH_GAIN).FinishRun()
+
         assert Pix + Samp == 1915
+        assert np.sum(evt.nectarcam.tel[0].svc.pixel_ids) == 1719375
