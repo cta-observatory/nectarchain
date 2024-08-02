563c2128a86ad524c3f584381bb33afe50dc0c5a
Jean-Philippe Lenain <jlenain@in2p3.fr>
2024-08-02 23:35:38 +0200

    Streamline the WriteAllResults method

--- src/nectarchain/dqm/dqm_summary_processor.py
+++ src/nectarchain/dqm/dqm_summary_processor.py
@@ -32,47 +32,22 @@ class DQMSummary:
     ):
         print("Processor 5")
 
-    def WriteAllResults(self, path, DICT):
-        data2 = Table()
-        data1 = Table()
-        data0 = Table()
+    @staticmethod
+    def _create_hdu(name, content):
         data = Table()
-        hdu, hdu0, hdu1, hdu2 = None, None, None, None
+        data[name] = content
+        hdu = fits.BinTableHDU(data)
+        hdu.name = name
+        return hdu
+
+    def WriteAllResults(self, path, DICT):
         hdulist = fits.HDUList()
         for i, j in DICT.items():
-            if i == "Results_TriggerStatistics":
-                for n2, m2 in j.items():
-                    data2[n2] = m2
-                    hdu2 = fits.BinTableHDU(data2)
-                    hdu2.name = n2
-                    hdulist.append(hdu2)
-
-            elif (i == "Results_MeanWaveForms_HighGain") or (
-                i == "Results_MeanWaveForms_LowGain"
-            ):
-                for n1, m1 in j.items():
-                    data1[n1] = m1
-                    hdu1 = fits.BinTableHDU(data1)
-                    hdu1.name = n1
-                    hdulist.append(hdu1)
-
-            elif (i == "Results_PixelTimeline_HighGain") or (
-                i == "Results_PixelTimeline_LowGain"
-            ):
-                for n0, m0 in j.items():
-                    data0[n0] = m0
-                    hdu0 = fits.BinTableHDU(data0)
-                    hdulist.append(hdu0)
-
-            else:
-                for n, m in j.items():
-                    data[n] = m
-                    hdu = fits.BinTableHDU(data)
-                    hdu.name = n
-                    hdulist.append(hdu)
+            for name, content in j.items():
+                hdu = self._create_hdu(name, content)
+                hdulist.append(hdu)
 
         FileName = path + "_Results.fits"
         print(FileName)
         hdulist.writeto(FileName, overwrite=True)
         hdulist.info()
-        return None
