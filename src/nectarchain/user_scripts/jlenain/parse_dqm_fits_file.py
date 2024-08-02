89fdf4c7aaabadea39c0864dac610a6348d1c102
Jean-Philippe Lenain <jlenain@in2p3.fr>
2024-08-02 23:16:17 +0200

    Add exceptions, remove DQM results at end of script

--- src/nectarchain/user_scripts/jlenain/parse_dqm_fits_file.py
+++ src/nectarchain/user_scripts/jlenain/parse_dqm_fits_file.py
@@ -7,9 +7,12 @@ VM, once the DQM has run on DIRAC
 import argparse
 import logging
 import os
+import shutil
 import sys
 import tarfile
+from pathlib import Path
 
+import ZEO
 from astropy.io import fits
 from DIRAC.Interfaces.API.Dirac import Dirac
 
@@ -57,11 +60,7 @@ if not os.path.exists(os.path.basename(lfn)):
 with tarfile.open(os.path.basename(lfn), "r") as tar:
     tar.extractall(".")
 
-fits_file = (
-    f"./NectarCAM_DQM_Run"
-    f"{args.run}/output/NectarCAM_Run{args.run}/NectarCAM_Run"
-    f"{args.run}_calib/NectarCAM_Run{args.run}_Results.fits"
-)
+fits_file = f"./NectarCAM_DQM_Run{args.run}/output/NectarCAM_Run{args.run}/NectarCAM_Run{args.run}_calib/NectarCAM_Run{args.run}_Results.fits"
 
 hdu = fits.open(fits_file)
 
@@ -77,6 +76,21 @@ for h in range(1, len(hdu)):
         keyname = hdu[extname].header[f"TTYPE{i+1}"]
         outdict[extname][keyname] = hdu[extname].data[keyname]
 
-db = DQMDB(read_only=False)
-db.insert(f"NectarCAM_Run{args.run}", outdict)
-db.commit_and_close()
+try:
+    db = DQMDB(read_only=False)
+    db.insert(f"NectarCAM_Run{args.run}", outdict)
+    db.commit_and_close()
+except ZEO.Exceptions.ClientDisconnected as e:
+    logger.critical(f"Impossible to feed the ZODB data base. Received error: {e}")
+
+# Remove DQM archive file and directory
+try:
+    os.remove(f"NectarCAM_DQM_Run{args.run}.tar.gz")
+except OSError:
+    logger.warning(
+        f"Could not remove NectarCAM_DQM_Run{args.run}.tar.gz or it does not exist"
+    )
+
+dirpath = Path(f"./NectarCAM_DQM_Run{args.run}")
+if dirpath.exists() and dirpath.is_dir():
+    shutil.rmtree(dirpath)
