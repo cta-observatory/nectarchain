75db866d11e2c3c229ce6cc7df0ae5498bdb4788
Jean-Philippe Lenain <jlenain@in2p3.fr>
2024-08-21 14:56:30 +0200

    Forgot to switch back to pydata sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -119,9 +119,9 @@ if not version_match or version_match.isdigit():
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = "pydata_sphinx_theme"
+html_theme = "pydata_sphinx_theme"
 # html_theme = "alabaster"
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = f"{project}doc"
