d059b4d3713f03739667a718d658911237d12e05
Jean-Philippe Lenain <jlenain@in2p3.fr>
2024-09-30 11:55:24 +0200

    Cleaning

--- src/nectarchain/user_scripts/jlenain/dqm_job_submitter/cronjob_launchDQM.sh
+++ src/nectarchain/user_scripts/jlenain/dqm_job_submitter/cronjob_launchDQM.sh
@@ -16,19 +16,19 @@ localParentDir="/data/nvme/ZFITS"
 remoteParentDir="/vo.cta.in2p3.fr/nectarcam"
 nectarchainScriptDir="$HOME/local/src/python/cta-observatory/nectarchain/src/nectarchain/user_scripts/jlenain/dqm_job_submitter"
 
-cd $nectarchainScriptDir
+cd $nectarchainScriptDir || (echo "Failed to cd into ${nectarchainScriptDir}, exiting..."; exit 1)
 
 for run in $(find ${localParentDir} -type f -name "NectarCAM*.fits.fz" | awk -F. '{print $2}' | awk -Fn '{print $2}' | sort | uniq); do
     echo "Probing files for run ${run}"
     nbLocalFiles=$(find ${localParentDir} -type f -name "NectarCAM.Run${run}.????.fits.fz" | wc -l)
     echo "  Found $nbLocalFiles local files for run $run"
-    nbRemoteFiles=$(dfind $remoteParentDir | grep -e "NectarCAM.Run${run}" | grep -e "fits.fz" | wc -l)
+    nbRemoteFiles=$(dfind ${remoteParentDir} | grep -e "NectarCAM.Run${run}" | grep --count -e "fits.fz")
     echo "  Found $nbRemoteFiles remote files on DIRAC for run $run"
     # If number of local and remote files matching, will attempt to launch a DQM run
-    if [ $nbLocalFiles -eq $nbRemoteFiles ]; then
+    if [ ${nbLocalFiles} -eq ${nbRemoteFiles} ]; then
         echo "  Run $run: number of local and remote files matching, will attempt to submit a DQM job"
         # Has this DQM run already been submitted ?
-        if [ $(dstat | grep -e "NectarCAM DQM run ${run}" | wc -l) -eq 0 ]; then
+        if [ $(dstat | grep --count -e "NectarCAM DQM run ${run}") -eq 0 ]; then
             yyyymmdd=$(find ${localParentDir} -type f -name "NectarCAM.Run${run}.????.fits.fz" | head -n 1 | awk -F/ '{print $6}')
             yyyy=${yyyymmdd:0:4}
             mm=${yyyymmdd:4:2}
