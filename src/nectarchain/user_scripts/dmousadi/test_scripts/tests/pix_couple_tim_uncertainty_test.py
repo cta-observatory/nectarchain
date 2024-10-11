ec3fd68d30ebd945b0128e198814a36d63059043
MOUSADI Despoina <despoina.mousadi@cea.fr>
2024-10-11 11:40:20 +0200

    pre-commit

--- src/nectarchain/user_scripts/dmousadi/test_scripts/tests/pix_couple_tim_uncertainty_test.py
+++ src/nectarchain/user_scripts/dmousadi/test_scripts/tests/pix_couple_tim_uncertainty_test.py
@@ -1,33 +1,65 @@
+import argparse
+import os
+import pickle
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
 from test_tools_components import ToMPairsTool
-import os
-import sys
-import pickle
-
-import argparse
 
 
 def get_args():
     """
     Parses command-line arguments for the pix_couple_tim_uncertainty_test.py script.
-    
+
     Returns:
         argparse.ArgumentParser: The parsed command-line arguments.
     """
-    parser = argparse.ArgumentParser(description='Time resolution (timing uncertainty between couples of pixels) test B-TEL-1030.\n'
-                                    +'According to the nectarchain component interface, you have to set a NECTARCAMDATA environment variable in the folder where you have the data from your runs or where you want them to be downloaded.\n'
-                                    +'You have to give a list of runs (run numbers with spaces inbetween) and an output directory to save the final plot.\n'
-                                    +'If the data is not in NECTARCAMDATA, the files will be downloaded through DIRAC.\n For the purposes of testing this script, default data is from the runs used for this test in the TRR document.\n'
-                                    +'You can optionally specify the number of events to be processed (default 1000). Takes a lot of time.\n')
-    parser.add_argument('-r','--runlist', type=int, nargs='+', help='List of runs (numbers separated by space). You can put just one run, default 3292', required=False, default = [3292])
-    parser.add_argument('-e','--evts', type = int, help='Number of events to process from each run. Default is 100. 1000 or more gives best results but takes some time', required=False, default=100)
-    parser.add_argument('-t','--pmt_transit_time', type=str, help='.csv file with pmt transit time corrections', required=False, default="/Users/dm277349/nectarchain_data/transit_time/hv_pmt_tom_correction_laser_measurement_per_pixel_fit_sqrt_hv_newmethod.csv")
-    parser.add_argument('-o','--output', type=str, help='Output directory. If none, plot will be saved in current directory', required=False, default='./')
-    parser.add_argument("--temp_output", help="Temporary output directory for GUI", default=None)
-    
-    return parser
+    parser = argparse.ArgumentParser(
+        description="Time resolution (timing uncertainty between couples of pixels) test B-TEL-1030.\n"
+        + "According to the nectarchain component interface, you have to set a NECTARCAMDATA environment variable in the folder where you have the data from your runs or where you want them to be downloaded.\n"
+        + "You have to give a list of runs (run numbers with spaces inbetween) and an output directory to save the final plot.\n"
+        + "If the data is not in NECTARCAMDATA, the files will be downloaded through DIRAC.\n For the purposes of testing this script, default data is from the runs used for this test in the TRR document.\n"
+        + "You can optionally specify the number of events to be processed (default 1000). Takes a lot of time.\n"
+    )
+    parser.add_argument(
+        "-r",
+        "--runlist",
+        type=int,
+        nargs="+",
+        help="List of runs (numbers separated by space). You can put just one run, default 3292",
+        required=False,
+        default=[3292],
+    )
+    parser.add_argument(
+        "-e",
+        "--evts",
+        type=int,
+        help="Number of events to process from each run. Default is 100. 1000 or more gives best results but takes some time",
+        required=False,
+        default=100,
+    )
+    parser.add_argument(
+        "-t",
+        "--pmt_transit_time",
+        type=str,
+        help=".csv file with pmt transit time corrections",
+        required=False,
+        default="../transit_time/hv_pmt_tom_correction_laser_measurement_per_pixel_fit_sqrt_hv_newmethod.csv",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Output directory. If none, plot will be saved in current directory",
+        required=False,
+        default="./",
+    )
+    parser.add_argument(
+        "--temp_output", help="Temporary output directory for GUI", default=None
+    )
 
+    return parser
 
 
 def main():
@@ -41,11 +73,8 @@ def main():
     If a temporary output directory is specified, the plot is also saved to a pickle file in that directory for the gui to use.
     """
 
-    
     parser = get_args()
     args = parser.parse_args()
-
-
 
     tt_path = "/Users/dm277349/nectarchain_data/transit_time/hv_pmt_tom_correction_laser_measurement_per_pixel_fit_sqrt_hv_newmethod.csv"
 
@@ -58,11 +87,10 @@ def main():
     print(f"Output directory: {output_dir}")  # Debug print
     print(f"Temporary output file: {temp_output}")  # Debug print
 
-
     sys.argv = sys.argv[:1]
     tom = []
 
-    pixel_ids = []  #pixel ids for run
+    pixel_ids = []  # pixel ids for run
 
     tom_corrected = []
 
@@ -70,12 +98,17 @@ def main():
     dt_corrected = []
     pixel_pairs = []
 
-
     for run in runlist:
-        
         print("PROCESSING RUN {}".format(run))
         tool = ToMPairsTool(
-            progress_bar=True, run_number=run, events_per_slice = 501 ,max_events=nevents, log_level=20, peak_height=10, window_width=16, overwrite=True
+            progress_bar=True,
+            run_number=run,
+            events_per_slice=501,
+            max_events=nevents,
+            log_level=20,
+            peak_height=10,
+            window_width=16,
+            overwrite=True,
         )
         tool.initialize()
         tool.setup()
@@ -87,44 +120,51 @@ def main():
         dt_no_correction.append(output[3])
         dt_corrected.append(output[4])
         pixel_pairs.append(output[5])
-        
 
     dt_no_correction = np.array(dt_no_correction[0])
     dt_corrected = np.array(output[4])
-    dt_corrected[abs(dt_corrected)>7]=np.nan
-    dt_corrected[abs(dt_no_correction)>7]=np.nan
-    std_corrected = np.nanstd(dt_corrected, axis = 1)
+    dt_corrected[abs(dt_corrected) > 7] = np.nan
+    dt_corrected[abs(dt_no_correction) > 7] = np.nan
+    std_corrected = np.nanstd(dt_corrected, axis=1)
 
-    fig, ax = plt.subplots(figsize=(10,10/1.61))
-    plt.hist(std_corrected,range=(0,5),density=True, 
-                histtype='step', lw=3, bins=200)
+    fig, ax = plt.subplots(figsize=(10, 10 / 1.61))
+    plt.hist(std_corrected, range=(0, 5), density=True, histtype="step", lw=3, bins=200)
 
-    plt.axvline(2, color='C4', alpha=0.8)
-    ax.text(2.1, 0.5, 'CTA requirement', color='C4', fontsize=20,
-            rotation=-90)
+    plt.axvline(2, color="C4", alpha=0.8)
+    ax.text(2.1, 0.5, "CTA requirement", color="C4", fontsize=20, rotation=-90)
 
+    ax.annotate(
+        "",
+        xy=(1.6, 0.25),
+        xytext=(2, 0.25),
+        color="C4",
+        alpha=0.5,
+        transform=ax.transAxes,
+        arrowprops=dict(color="C4", alpha=0.7, lw=3, arrowstyle="->"),
+    )
 
-    ax.annotate("", xy=(1.6, 0.25), xytext=(2, 0.25), color='C4', alpha=0.5,
-                transform=ax.transAxes,
-                arrowprops=dict(color='C4', alpha=0.7, lw=3, arrowstyle='->'))
+    ax.annotate(
+        "",
+        xy=(1.6, 0.75),
+        xytext=(2, 0.75),
+        color="C4",
+        alpha=0.5,
+        transform=ax.transAxes,
+        arrowprops=dict(color="C4", alpha=0.7, lw=3, arrowstyle="->"),
+    )
 
-    ax.annotate("", xy=(1.6, 0.75), xytext=(2, 0.75), color='C4', alpha=0.5,
-                transform=ax.transAxes,
-                arrowprops=dict(color='C4', alpha=0.7, lw=3, arrowstyle='->'))
+    plt.xlabel("RMS of $\Delta t_{\mathrm{TOM}}$ for pairs of pixels [ns]")
+    plt.ylabel("Normalized entries")
 
-    plt.xlabel('RMS of $\Delta t_{\mathrm{TOM}}$ for pairs of pixels [ns]')
-    plt.ylabel('Normalized entries')
-    
     plt.gcf()
 
-    plt.savefig(os.path.join(output_dir,"pix_couple_tim_uncertainty.png"))
+    plt.savefig(os.path.join(output_dir, "pix_couple_tim_uncertainty.png"))
 
     if temp_output:
-        with open(os.path.join(args.temp_output, 'plot1.pkl'), 'wb') as f:
+        with open(os.path.join(args.temp_output, "plot1.pkl"), "wb") as f:
             pickle.dump(fig, f)
-    
-    plt.close('all')
 
+    plt.close("all")
 
 
 if __name__ == "__main__":
