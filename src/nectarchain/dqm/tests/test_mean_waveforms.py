845b3c0f6e6c10057433c3eec26867f2cb45f57a
hashkar <43830561+hashkar@users.noreply.github.com>
2024-11-06 13:43:55 +0100

    Dqm update - using standard ctapipe tools (#154)
    * modified charge intergration using ctapipe charge extraction methods
    
    * adjusting defaults
    
    * fixing camera plotting in charge_integration
    
    * switching from LocalPeakWindowSum to GlobalPeakWindowSum as default
    
    * Simplify existing unit tests
    
    ---------
    
    Co-authored-by: jlenain <jlenain@in2p3.fr>

--- src/nectarchain/dqm/tests/test_mean_waveforms.py
+++ src/nectarchain/dqm/tests/test_mean_waveforms.py
@@ -9,14 +9,8 @@ from nectarchain.dqm.mean_waveforms import MeanWaveFormsHighLowGain
 
 
 class TestMeanWaveForms:
-    run_number = 3798
-    max_events = 1
-
     def test_mean_waveforms(self):
-        # run_number = 3938
         path = get_dataset_path("NectarCAM.Run3938.30events.fits.fz")
-
-        config = None
 
         config = Config(
             dict(
@@ -34,12 +28,9 @@ class TestMeanWaveForms:
 
         Pix, Samp = MeanWaveFormsHighLowGain(HIGH_GAIN).DefineForRun(reader1)
 
-        MeanWaveFormsHighLowGain(HIGH_GAIN).ConfigureForRun(path, Pix, Samp, reader1)
-
+        evt = None
         for evt in tqdm(reader1, total=1):
             self.pixelBAD = evt.mon.tel[0].pixel_status.hardware_failing_pixels
-            # MeanWaveFormsHighLowGain(HIGH_GAIN).ProcessEvent(evt, noped = False)
-        # MeanWaveFormsHighLowGain(HIGH_GAIN).FinishRun()
-        assert Pix + Samp == 1915
 
+        assert Pix + Samp == 1915
         assert np.sum(evt.r0.tel[0].waveform[HIGH_GAIN][0]) == 3932100
--- src/nectarchain/dqm/tests/test_pixel_timeline.py
+++ src/nectarchain/dqm/tests/test_pixel_timeline.py
@@ -9,14 +9,8 @@ from nectarchain.dqm.pixel_timeline import PixelTimelineHighLowGain
 
 
 class TestPixelTimeline:
-    run_number = 3798
-    max_events = 1
-
     def test_pixel_timeline(self):
-        # run_number = 3938
         path = get_dataset_path("NectarCAM.Run3938.30events.fits.fz")
-
-        config = None
 
         config = Config(
             dict(
@@ -34,13 +28,9 @@ class TestPixelTimeline:
 
         Pix, Samp = PixelTimelineHighLowGain(HIGH_GAIN).DefineForRun(reader1)
 
-        PixelTimelineHighLowGain(HIGH_GAIN).ConfigureForRun(path, Pix, Samp, reader1)
-
+        evt = None
         for evt in tqdm(reader1, total=1):
             self.pixelBAD = evt.mon.tel[0].pixel_status.hardware_failing_pixels
-            # PixelTimelineHighLowGain(HIGH_GAIN).ProcessEvent(evt, noped = False)
-
-        PixelTimelineHighLowGain(HIGH_GAIN).FinishRun()
 
         assert Pix + Samp == 1915
         assert np.sum(evt.nectarcam.tel[0].svc.pixel_ids) == 1719375
