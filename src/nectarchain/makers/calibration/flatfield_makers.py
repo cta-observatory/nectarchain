a7f6b5233ee8df6aceb87f57d70c51ae95256a13
ArmelleJB <armelle.jardin-blicq@lp2ib.in2p3.fr>
2024-11-11 09:34:40 -0300

    Update flat field tools

--- src/nectarchain/makers/calibration/flatfield_makers.py
+++ src/nectarchain/makers/calibration/flatfield_makers.py
@@ -10,9 +10,29 @@ log.handlers = logging.getLogger("__main__").handlers
 __all__ = ["FlatfieldNectarCAMCalibrationTool"]
 
 
-class FlatfieldNectarCAMCalibrationTool(NectarCAMCalibrationTool):
-    def start(self):
-        raise NotImplementedError(
-            "The computation of the flatfield calibration is not yet implemented, \
-                feel free to contribute !:)"
+#class FlatfieldNectarCAMCalibrationTool(NectarCAMCalibrationTool):
+#    def start(self):
+#        raise NotImplementedError(
+#            "The computation of the flatfield calibration is not yet implemented, feel free to contribute !:)"
+#        )
+
+from nectarchain.makers import EventsLoopNectarCAMCalibrationTool
+
+
+class FlatfieldNectarCAMCalibrationTool(EventsLoopNectarCAMCalibrationTool):
+    name = "NectarCAM"
+
+    componentsList = ComponentNameList(
+        NectarCAMComponent,
+        default_value=["preFlatFieldComponent"],
+        help="List of Component names to be apply, the order will be respected",
+    ).tag(config=True)
+
+    def _init_output_path(self):
+        if self.max_events is None:
+            filename = f"{self.name}_run{self.run_number}.h5"
+        else:
+            filename = f"{self.name}_run{self.run_number}_maxevents{self.max_events}.h5"
+        self.output_path = pathlib.Path(
+            f"{os.environ.get('NECTARCAMDATA','/tmp')}/tutorials/{filename}"
         )
