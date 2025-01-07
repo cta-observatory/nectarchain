c5d4419e41fbfa82a021310418931d167285d91b
guillaume.grolleron <ggroller@lpnhe.in2p3.fr>
2025-01-07 18:50:05 +0100

    static method decorator were missing

--- src/nectarchain/data/management.py
+++ src/nectarchain/data/management.py
@@ -262,12 +262,12 @@ class DataManagement:
             return lfns
         else:
             return url_data
-
+    @staticmethod
     def find_waveforms(run_number, max_events=None):
         return __class__.__find_computed_data(
             run_number=run_number, max_events=max_events, data_type="waveforms"
         )
-
+    @staticmethod
     def find_charges(
         run_number, method="FullWaveformSum", str_extractor_kwargs="", max_events=None
     ):
@@ -277,7 +277,7 @@ class DataManagement:
             ext=f"_{method}_{str_extractor_kwargs}.h5",
             data_type="charges",
         )
-
+    @staticmethod
     def find_photostat(
         FF_run_number,
         ped_run_number,
@@ -296,7 +296,7 @@ class DataManagement:
         if len(full_file) != 1:
             raise Exception(f"the files is {full_file}")
         return full_file
-
+    @staticmethod
     def find_SPE_combined(
         run_number, method="FullWaveformSum", str_extractor_kwargs=""
     ):
@@ -306,7 +306,7 @@ class DataManagement:
             str_extractor_kwargs=str_extractor_kwargs,
             keyword="FlatFieldCombined",
         )
-
+    @staticmethod
     def find_SPE_nominal(
         run_number, method="FullWaveformSum", str_extractor_kwargs="", free_pp_n=False
     ):
@@ -317,7 +317,7 @@ class DataManagement:
             free_pp_n=free_pp_n,
             keyword="FlatFieldSPENominal",
         )
-
+    @staticmethod
     def find_SPE_HHV(
         run_number,
         method="FullWaveformSum",
@@ -357,7 +357,7 @@ class DataManagement:
             return [all_files[index]]
         else:
             return full_file
-
+    @staticmethod
     def __find_computed_data(
         run_number, max_events=None, ext=".h5", data_type="waveforms"
     ):
