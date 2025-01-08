4463bfa5de25cd57a82d080e9d5594cc5f87fcaf
guillaume.grolleron <ggroller@lpnhe.in2p3.fr>
2025-01-08 22:51:42 +0100

    dqm flake8

--- src/nectarchain/dqm/charge_integration.py
+++ src/nectarchain/dqm/charge_integration.py
@@ -1,15 +1,13 @@
 import ctapipe.instrument.camera.readout
 import numpy as np
 from ctapipe.coordinates import EngineeringCameraFrame
-from ctapipe.image.extractor import (
-    FixedWindowSum,
-    FullWaveformSum,
-    GlobalPeakWindowSum,
-    LocalPeakWindowSum,
-    NeighborPeakWindowSum,
-    SlidingWindowMaxSum,
-    TwoPassWindowSum,
-)
+from ctapipe.image.extractor import FixedWindowSum  # noqa
+from ctapipe.image.extractor import FullWaveformSum  # noqa
+from ctapipe.image.extractor import GlobalPeakWindowSum  # noqa
+from ctapipe.image.extractor import LocalPeakWindowSum  # noqa
+from ctapipe.image.extractor import NeighborPeakWindowSum  # noqa
+from ctapipe.image.extractor import SlidingWindowMaxSum  # noqa
+from ctapipe.image.extractor import TwoPassWindowSum  # noqa
 from ctapipe.visualization import CameraDisplay
 from ctapipe_io_nectarcam import constants
 from matplotlib import pyplot as plt
