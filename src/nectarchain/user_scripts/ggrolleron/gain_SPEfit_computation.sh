546a6106f2a921fd87c366dd38828f1460cf321b
guillaume.grolleron <ggroller@lpnhe.in2p3.fr>
2025-02-07 17:25:42 +0100

    update of shell scripts

--- src/nectarchain/user_scripts/ggrolleron/gain_SPEfit_computation.sh
+++ src/nectarchain/user_scripts/ggrolleron/gain_SPEfit_computation.sh
@@ -4,7 +4,7 @@
 #nominal runs : 2633,3936
 
 #to perform SPE fit of a run at nominal voltage
-python gain_SPEfit_computation.py -r 3942 --HHV --reload_events --multiproc --nproc 8 --chunksize 2 -p 45 56 --max_events 100  --method LocalPeakWindowSum --extractor_kwargs '{"window_width":16,"window_shift":4}' --overwrite -v DEBUG
+python gain_SPEfit_computation.py -r 3942 --reload_events --multiproc --nproc 8 --chunksize 2 -p 45 56 --max_events 100  --method LocalPeakWindowSum --extractor_kwargs '{"window_width":16,"window_shift":4}' --overwrite -v DEBUG
 #to perform SPE fit of a HHV run 
 python gain_SPEfit_computation.py -r 3936 --reload_events --multiproc --nproc 8 --chunksize 2 -p 45 56 --max_events 100  --method LocalPeakWindowSum --extractor_kwargs '{"window_width":16,"window_shift":4}' --overwrite -v DEBUG
 #to perform SPE fit of a HHV run letting n and pp parameters free
