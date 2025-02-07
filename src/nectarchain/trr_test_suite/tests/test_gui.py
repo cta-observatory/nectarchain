8f18c4562d4501b610bf78290454e05adb6060c1
guillaume.grolleron <ggroller@lpnhe.in2p3.fr>
2025-02-07 21:36:46 +0100

    change test_gui to work on macos

--- src/nectarchain/trr_test_suite/tests/test_gui.py
+++ src/nectarchain/trr_test_suite/tests/test_gui.py
@@ -1,23 +1,36 @@
-from unittest.mock import MagicMock, patch
+import unittest
+from unittest.mock import patch
 
 from nectarchain.trr_test_suite import TestRunner
 
-try:
-    import PyQt6  # noqa: F401
 
-    pyqt_module = "PyQt6"
-except ImportError:
-    import PyQt5  # noqa: F401
-
-    pyqt_module = "PyQt5"
-
-
-class TestTestRunner:
-    @patch(f"{pyqt_module}.QtWidgets.QWidget.__init__", return_value=None)
-    @patch(f"{pyqt_module}.QtWidgets.QApplication", MagicMock())
+class TestTestRunner(unittest.TestCase):
     @patch("nectarchain.trr_test_suite.gui.TestRunner.init_ui", return_value=None)
-    def test_init(self, mock_init_ui, mock_qwidget):
-        runner = TestRunner()
-        assert isinstance(runner, TestRunner)
-        mock_qwidget.assert_called_once()
-        mock_init_ui.assert_called_once()
+    def test_init_pyqt(self, mock_init_ui):
+        try:
+            import PyQt6  # noqa: F401
+
+            with patch(
+                "PyQt6.QtWidgets.QWidget.__init__", return_value=None
+            ) as mock_qwidget:
+                runner = TestRunner()
+                assert isinstance(runner, TestRunner)
+                mock_qwidget.assert_called_once()
+                mock_init_ui.assert_called_once()
+        except ImportError:
+            self.skipTest("PyQt6 is not available")
+
+    @patch("nectarchain.trr_test_suite.gui.TestRunner.init_ui", return_value=None)
+    def test_init_pyqt5(self, mock_init_ui):
+        try:
+            import PyQt5  # noqa: F401
+
+            with patch(
+                "PyQt5.QtWidgets.QWidget.__init__", return_value=None
+            ) as mock_qwidget:
+                runner = TestRunner()
+                assert isinstance(runner, TestRunner)
+                mock_qwidget.assert_called_once()
+                mock_init_ui.assert_called_once()
+        except ImportError:
+            self.skipTest("PyQt5 is not available")
