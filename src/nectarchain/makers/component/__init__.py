4dd58db954bd865f6b8553244f592f48e268fd1f
ArmelleJB <armelle.jardin-blicq@lp2ib.in2p3.fr>
2025-02-17 13:40:39 -0300

    update the flat field component name

--- src/nectarchain/makers/component/__init__.py
+++ src/nectarchain/makers/component/__init__.py
@@ -11,7 +11,7 @@ from .gain_component import GainNectarCAMComponent
 from .pedestal_component import PedestalEstimationComponent
 from .photostatistic_algorithm import PhotoStatisticAlgorithm
 from .photostatistic_component import PhotoStatisticNectarCAMComponent
-from .preflatfield_component import PreFlatFieldComponent
+from .flatfield_component import FlatFieldComponent
 from .spe import (
     SPECombinedalgorithm,
     SPEHHValgorithm,
