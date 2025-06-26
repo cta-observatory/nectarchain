d26bcffb52ddec5ff941f49637775073a937eb63
jlenain <jlenain@in2p3.fr>
2025-06-26 16:11:20 +0200

    Revert from main

--- src/nectarchain/dqm/pixel_participation.py
+++ src/nectarchain/dqm/pixel_participation.py
@@ -1,5 +1,3 @@
-import os
-
 import numpy as np
 from ctapipe.coordinates import EngineeringCameraFrame
 from ctapipe.visualization import CameraDisplay
@@ -20,22 +18,12 @@ class PixelParticipationHighLowGain(DQMSummary):
         self.BadPixels_ped = None
         self.BadPixels = None
         self.camera = None
+        self.camera2 = None
+        self.cmap = None
+        self.cmap2 = None
         self.PixelParticipation_Results_Dict = {}
         self.PixelParticipation_Figures_Dict = {}
         self.PixelParticipation_Figures_Names_Dict = {}
-
-        gain_c = "High" if gaink == 0 else "Low"
-        self.gain_c = gain_c
-
-        self.figure_keys = {
-            "ped": f"CAMERA-BADPIX-PED-DISPLAY-{gain_c}-GAIN",
-            "phy": f"CAMERA-BADPIX-PHY-DISPLAY-{gain_c}-GAIN",
-        }
-
-        self.figure_filenames = {
-            "ped": f"_Pedestal_BPX_{gain_c}Gain.png",
-            "phy": f"_Camera_BPX_{gain_c}Gain.png",
-        }
 
     def ConfigureForRun(self, path, Pix, Samp, Reader1, **kwargs):
         # define number of pixels and samples
@@ -50,21 +38,33 @@ class PixelParticipationHighLowGain(DQMSummary):
             EngineeringCameraFrame()
         )
 
+        self.camera2 = Reader1.subarray.tel[0].camera.geometry.transform_to(
+            EngineeringCameraFrame()
+        )
+
+        self.cmap = "gnuplot2"
+        self.cmap2 = "gnuplot2"
+
     def ProcessEvent(self, evt, noped):
         pixelBAD = evt.mon.tel[0].pixel_status.hardware_failing_pixels[self.k]
         pixel = evt.nectarcam.tel[0].svc.pixel_ids
-
-        # Create a full-size zero-initialized array for indexing safety
-        status = np.zeros(self.Pix, dtype=int)
-        np.put(status, pixel, pixelBAD[pixel])
+        if len(pixel) < self.Pix:
+            pixel21 = list(np.arange(0, self.Pix - len(pixel), 1, dtype=int))
+            pixel = list(pixel)
+            pixels = np.concatenate([pixel21, pixel])
+        else:
+            pixels = pixel
 
         if evt.trigger.event_type.value == 32:  # count peds
             self.counter_ped += 1
-            self.BadPixels_ped += status
+            BadPixels_ped1 = list(map(int, pixelBAD[pixels]))
+            self.BadPixels_ped += BadPixels_ped1
 
         else:
             self.counter_evt += 1
-            self.BadPixels += status
+            BadPixels1 = list(map(int, pixelBAD[pixels]))
+            self.BadPixels += BadPixels1
+        return None
 
     def FinishRun(self):
         self.BadPixels_ped = np.array(self.BadPixels_ped)
@@ -97,36 +97,52 @@ class PixelParticipationHighLowGain(DQMSummary):
         return self.PixelParticipation_Results_Dict
 
     def PlotResults(self, name, FigPath):
-        for key, data, count in [
-            ("phy", self.BadPixels, self.counter_evt),
-            ("ped", self.BadPixels_ped, self.counter_ped),
-        ]:
-            if count == 0:
-                continue
+        # titles = ['All', 'Pedestals']
+        if self.k == 0:
+            gain_c = "High"
+        if self.k == 1:
+            gain_c = "Low"
 
-            fig, disp = plt.subplots()
+        if self.counter_evt > 0:
+            entity = self.BadPixels
+            title = "Camera BPX %s gain (ALL)" % gain_c
 
-            title = (
-                f"Camera BPX {self.gain_c} gain ({'ALL' if key == 'phy' else 'PED'})"
-            )
+        if self.counter_ped > 0:
+            entity = self.BadPixels_ped
+            title = "Camera BPX %s gain (PED)" % gain_c
 
-            disp = CameraDisplay(
-                geometry=self.camera,
-                image=data,
-                cmap=plt.cm.coolwarm,
-            )
-            disp.add_colorbar()
-            disp.axes.text(2.0, 0, "Bad Pixels", rotation=90)
-            plt.title(title)
+        fig, disp = plt.subplots()
+        disp = CameraDisplay(
+            geometry=self.camera,
+            image=entity,
+            cmap=self.cmap,
+        )
+        disp.cmap = self.cmap
+        disp.cmap = plt.cm.coolwarm
+        disp.add_colorbar()
+        disp.axes.text(2.0, 0, "Bad Pixels", rotation=90)
+        plt.title(title)
 
-            fig_key = self.figure_keys[key]
-            fig_name = name + self.figure_filenames[key]
-            fig_path = os.path.join(FigPath, fig_name)
+        if self.counter_ped > 0:
+            self.PixelParticipation_Figures_Dict[
+                "CAMERA-BADPIX-PHY-DISPLAY-%s-GAIN" % gain_c
+            ] = fig
+            full_name = name + "_Camera_BPX_%sGain.png" % gain_c
+            FullPath = FigPath + full_name
+            self.PixelParticipation_Figures_Names_Dict[
+                "CAMERA-BADPIX-PHY-DISPLAY-%s-GAIN" % gain_c
+            ] = FullPath
+        if self.counter_evt > 0:
+            self.PixelParticipation_Figures_Dict[
+                "CAMERA-BADPIX-PED-DISPLAY-%s-GAIN" % gain_c
+            ] = fig
+            full_name = name + "_Pedestal_BPX_%sGain.png" % gain_c
+            FullPath = FigPath + full_name
+            self.PixelParticipation_Figures_Names_Dict[
+                "CAMERA-BADPIX-PED-DISPLAY-%s-GAIN" % gain_c
+            ] = FullPath
 
-            self.PixelParticipation_Figures_Dict[fig_key] = fig
-            self.PixelParticipation_Figures_Names_Dict[fig_key] = fig_path
-
-            plt.close(fig)
+            plt.close()
 
         return (
             self.PixelParticipation_Figures_Dict,
