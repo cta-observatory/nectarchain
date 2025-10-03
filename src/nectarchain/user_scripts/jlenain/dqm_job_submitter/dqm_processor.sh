e48302d1809619e2ca9db5f3d75846d70fa7f825
Jean-Philippe Lenain <jlenain@in2p3.fr>
2025-10-03 16:32:24 +0200

    Propagate camera option to DQM job submitter

--- src/nectarchain/user_scripts/jlenain/dqm_job_submitter/dqm_processor.sh
+++ src/nectarchain/user_scripts/jlenain/dqm_job_submitter/dqm_processor.sh
@@ -114,7 +114,7 @@ echo
 echo "Running" 
 # Instantiate the nectarchain Singularity image, run our DQM example run within it:
 # cmd="\$CALLER exec --home $PWD $CONTAINER /opt/conda/envs/nectarchain/bin/python /opt/cta/nectarchain/src/nectarchain/dqm/start_dqm.py --r0 $NECTARCAMDATA $NECTARDIR -i $LISTRUNS"
-cmd="\$CALLER exec --home $PWD $CONTAINER /opt/conda/envs/nectarchain/bin/python /opt/cta/nectarchain/src/nectarchain/dqm/start_dqm.py --r0 --runnb $runnb $NECTARCAMDATA $NECTARDIR"
+cmd="\$CALLER exec --home $PWD $CONTAINER /opt/conda/envs/nectarchain/bin/python /opt/cta/nectarchain/src/nectarchain/dqm/start_dqm.py --r0 --runnb $runnb --camera $camera $NECTARCAMDATA $NECTARDIR"
 echo \$cmd
 eval \$cmd
 EOF
