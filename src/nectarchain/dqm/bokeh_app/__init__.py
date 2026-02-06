9e9487466fee3b86b75080a5b53c84082ccdb2b0
A_Montanari <49238256+alessandromontanari@users.noreply.github.com>
2026-02-06 16:37:38 +0100

    Implement python logging in Bokeh app to log app logic (#256)
    * feat(src/nectarchain/dqm/bokeh_app):
    
    - logging_config.py: setup_logger for RotatingFileHandler saving in logs/app.log
    - app_hooks.py: use logger instead of print and add new log messages
    - main.py: use logger instead of print and add new log messages
    
    * refactor log_dir path to NECTARCHAIN_LOG if defined, otherwise default
    
    * Properly expose `logging_config.setup_logger`, to be used in unit tests
    
    ---------
    
    Co-authored-by: Jean-Philippe Lenain <jlenain@in2p3.fr>

---
+++ src/nectarchain/dqm/bokeh_app/__init__.py
@@ -0,0 +1,5 @@
+from .logging_config import setup_logger
+
+__all__ = [
+    "setup_logger",
+]
---
+++ src/nectarchain/dqm/bokeh_app/logging_config.py
@@ -0,0 +1,54 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logger(logger_name="nectarchain.dqm.bokeh_app", log_level=logging.INFO):
+    """Set up a rotating file logger for the Bokeh app
+
+    Parameters
+    ----------
+    logger_name : str, optional
+        Name of the logger, by default 'nectarchain.dqm.bokeh_app'
+    log_level : int, optional
+        Logging level (e.g., logging.INFO, logging.DEBUG), by default logging.INFO
+
+    Returns
+    -------
+    logging.Logger
+        Configured logger
+    """
+
+    # either write the logs to $NECTARCHAIN_LOG,
+    # or to src/nectarchain/dqm/bokeh_app/logs as default.
+    log_dir = os.environ.get(
+        "NECTARCHAIN_LOG", os.path.join(os.path.dirname(__file__), "logs")
+    )
+    os.makedirs(log_dir, exist_ok=True)
+    log_file = os.path.join(log_dir, "app.log")
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(log_level)
+
+    # Avoid duplicate handlers if the function is called multiple times
+    if logger.handlers:
+        return logger
+
+    # Rotating file handler for when the app will be running continously
+    handler = RotatingFileHandler(
+        log_file,
+        maxBytes=5 * 1024 * 1024,  # 5 MB per log file
+        backupCount=3,  # Keep up to 3 backup log files
+        encoding="utf-8",
+    )
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - "
+        "%(funcName)s (line %(lineno)d) - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
