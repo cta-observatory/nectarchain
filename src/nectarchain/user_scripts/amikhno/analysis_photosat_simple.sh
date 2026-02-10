b49b73c40f8e39756fabad6ad9f500342eb3d5b0
Jean-Philippe Lenain <jlenain@in2p3.fr>
2026-02-10 11:08:08 +0100

    Uncomment line for SPE fit processing

--- src/nectarchain/user_scripts/amikhno/analysis_photosat_simple.sh
+++ src/nectarchain/user_scripts/amikhno/analysis_photosat_simple.sh
@@ -1,7 +1,8 @@
-#HOW TO:
-#In order to compute flat-field coefficients using the gain computed with photostatistincs method, one has to:
-#1) Run the SPEfit, example of bash script:
-#python gain_SPEfit_computation.py -r 6774 --reload_events --multiproc --nproc 8  --method LocalPeakWindowSum --extractor_kwargs '{"window_shift": 4, "window_width":8}' --overwrite -v INFO
-#2) Run the phototatistic method and the fit of 2D Gaussian model to get ff-coeficients
-
+# HOW TO:
+# In order to compute flat-field coefficients using the gain computed with
+# photostatistincs method, one has to:
+# 1) Run the SPEfit, example of bash script:
+python gain_SPEfit_computation.py -r 6774 --reload_events --multiproc --nproc 8  --method LocalPeakWindowSum --extractor_kwargs '{"window_shift": 4, "window_width":8}' --overwrite -v INFO
+# 2) Run the phototatistics method and the fit of the 2D Gaussian model to get
+# FF-coefficients
 python analysis_photostat_simple.py --FF_run_number 6729 --SPE_run_number 6774  --SPE_config nominal --method LocalPeakWindowSum --extractor_kwargs '{"window_shift": 4, "window_width":8}'
