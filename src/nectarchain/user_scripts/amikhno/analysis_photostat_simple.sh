8c536454bda0efa03007d3f36936d0d47c29a14b
Jean-Philippe Lenain <jlenain@in2p3.fr>
2026-02-13 14:25:37 +0100

    Changed example script to reflect the fact that we do not need to run the SPE fit manually any more, before performing the analysis of the characterization of the flat-field calibration source

--- src/nectarchain/user_scripts/amikhno/analysis_photostat_simple.sh
+++ src/nectarchain/user_scripts/amikhno/analysis_photostat_simple.sh
@@ -1,8 +1,5 @@
 # HOW TO:
-# In order to compute flat-field coefficients using the gain computed with
-# photostatistincs method, one has to:
-# 1) Run the SPEfit, example of bash script:
-python gain_SPEfit_computation.py -r 6774 --reload_events --multiproc --nproc 8  --method LocalPeakWindowSum --extractor_kwargs '{"window_shift": 4, "window_width":8}' --overwrite -v INFO
-# 2) Run the phototatistics method and the fit of the 2D Gaussian model to get
-# FF-coefficients
+# Example to compute flat-field coefficients using the gain using the
+# photostatistics method, including the fit of a 2D Gaussian model to account for a
+# non-uniform response of the flat-field calibration source:
 python analysis_photostat_simple.py --FF_run_number 6729 --SPE_run_number 6774  --SPE_config nominal --method LocalPeakWindowSum --extractor_kwargs '{"window_shift": 4, "window_width":8}'
