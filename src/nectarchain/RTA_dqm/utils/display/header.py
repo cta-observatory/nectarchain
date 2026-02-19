e96fbd753f94bb45c897de2598e193ba3eb110e1
Julian Hamo <julian.hamo@ijclab.in2p3.fr>
2026-02-19 10:07:20 +0100

    Add preliminary RTA DQM bokeh interface

---
+++ src/nectarchain/RTA_dqm/utils/display/header.py
@@ -0,0 +1,321 @@
+# Author: Julian Hamo
+# Contact mail: julian.hamo@ijclab.in2p3.fr
+
+"""
+This module stores the Bokeh webpage header helpers for the RTA of NectarCAM.
+"""
+
+# imports
+import os
+import time
+
+# Bokeh imports
+from bokeh.models import (
+    Select,
+    Div
+)
+from bokeh.layouts import (
+    column
+)
+
+# Bokeh RTA imports
+from ..update_helpers import (
+    start_periodic_updates,
+    stop_periodic_updates,
+    update_all_figures
+)
+from ..data_fetch_helpers import (
+    safe_close_file,
+    open_file_from_selection,
+    _get_latest_file
+)
+
+
+__all__ = ["make_select_run", "make_status_div", "make_header_menu"]
+
+
+def _list_runs(ressource_path, extension=".h5"):
+    """List the name of the files in the directory of path ```ressource_path```
+    with the format ```extension```.
+
+    Parameters
+    ----------
+    ressource_path : string
+        Path of the directory where to find the files to list.
+        Can be relative or absolute (careful if it is relative,
+        might be an issue for portability).
+    extension : string, optional
+        Extension of the format of files to be listed.
+        Default is .h5.
+
+    Returns
+    -------
+    filenames : array_like
+        Array of the names of the files fitting the requirements,
+        without the extension format.
+
+    Examples
+    --------
+    >>> ressource_path = "../../example_data"
+    >>> len(_list_runs(ressource_path))
+    5
+
+    """
+
+    try:
+        return [
+            item[:-len(extension)]
+            for item in os.listdir(ressource_path)
+            if item.endswith(extension)
+        ]
+    except Exception:
+        pass
+
+
+def _set_status_text(msgs, status_col=None):
+    """Helper to set status in header if status_col exists.
+
+    Parameters
+    ----------
+    msg_file : list of string
+        Text to update the file_div and time_div in status_col
+    status_col : column, optional
+        Bokeh column layout for the status of the webpage.
+        Default is None, meaning nothing is happening.
+
+    Returns
+    -------
+    out : None
+
+    """
+
+    try:
+        # check if status_col has text attribute to be updated
+        if hasattr(status_col, "children") and len(status_col.children) > 1:
+            for index in range(2):
+                child = status_col.children[index]
+                if hasattr(child, "text"):
+                    child.text = msgs[index]
+    # error raised
+    except Exception:
+        pass
+    return
+
+
+def _on_header_select_change(
+        attr, old, new,
+        fobj=None, fpath=None, ressource_path=None,
+        status_col=None,
+        real_time_tag=None, default_update_ms=None,
+        display_registry=None, widgets=None,
+        time_parentkeys=None, time_childkeys=None
+    ):
+    """Callback when header selector changes value.
+    Select run to be displayed or real time mode.
+
+    Parameters
+    ----------
+    attr, old, new :
+        Arguments for the Bokeh .on_change method
+    fobj : hdf5 file, optional
+        File to open, default is None.
+    fpath : string, optional
+        Path of fobj, default is None.
+    ressource_path : string, optional
+        Path of directory to browse for files, default is None.
+    status_col : Div, optional
+        Bokeh column with the divider for the status of the webpage.
+        Default is None.
+    real_time_tag: string, optional
+        Tag representing the real-time mode.
+        Stored in static.constants.json.
+        Default is None.
+    default_update_ms: int, optional
+        Refresh period in real-time mode.
+        Stored in static.constants.json.
+        Default is None.
+    display_registry : list
+        Storage of all the displays for later update.
+    widgets : dict
+        Storage of all the widgets for later use and update.
+    time_parentkeys : list of strings, optional
+        Parentkeys of data that can be time ordered.
+        Default is ``None``, meaning nothing to be sorted.
+    time_childkeys : list of strings, optional
+        Childkeys of data that can be time ordered.
+        Default is ``None``, meaning nothing to be sorted.
+
+    Returns
+    -------
+    out : hdf5 file, string
+        Opened file and its path.
+        Every failure results in None, None.
+
+    """
+
+    sel = new
+
+    # close previously opened non-latest file (if any)
+    # (for now consider only format handled i.e .h5 files)
+    try:
+        if fobj is not None:
+            safe_close_file(fobj)
+    except Exception:
+        pass
+
+    # open right file depending on the Select value
+    if sel == real_time_tag:
+        print(f"Real-time mode: {time.strftime('%H:%M:%S')}")
+        try:
+            # real time will have to be replaced by ...data_fetch_helpers.fetch_stream()
+            fobj, fpath = open_file_from_selection(
+                sel, ressource_path=ressource_path, real_time_tag=real_time_tag,
+            )
+            # Update and start periodic updates
+            update_all_figures(
+                fobj, display_registry=display_registry, widgets=widgets
+            )
+            widgets["PERIODIC_CB_ID"] = start_periodic_updates(
+                file=fobj,
+                display_registry=display_registry,
+                widgets=widgets,
+                status_col=status_col,
+                interval_ms=default_update_ms
+            )
+            # Display
+            _set_status_text(
+                [
+                    f"Loaded file: {fobj.filename}",
+                    f"Last update: {time.strftime('%H:%M:%S')}"
+                ],
+                status_col=status_col
+            )
+            return fobj, fpath
+        except Exception as e:
+            _set_status_text(
+                [f"Failed to start real-time mode: {e}"] * 2,
+                status_col=status_col
+            )
+            print("Failed to start real-time mode:", e)
+            return None, None
+    else:
+        print(f"Reading mode (filename: {sel}): {time.strftime('%H:%M:%S')}")
+        # open the selected file and update once (no periodic updates)'
+        try:
+            widgets["PERIODIC_CB_ID"] = stop_periodic_updates(widgets)
+            fobj, fpath = open_file_from_selection(
+                sel, ressource_path=ressource_path, real_time_tag=real_time_tag,
+                time_parentkeys=time_parentkeys, time_childkeys=time_childkeys
+            )
+            # issue with opening the specific file
+            if fobj is None:
+                _set_status_text(
+                    [f"Could not open selected file: {sel}"] * 2,
+                    status_col=status_col
+                )
+                return None, None
+            update_all_figures(
+                fobj, display_registry=display_registry, widgets=widgets
+            )
+            _set_status_text(
+                [
+                    f"Loaded file: {fobj.filename}",
+                    f"Last update: {time.strftime('%H:%M:%S')}"
+                ],
+                status_col=status_col
+            )
+            return fobj, fpath
+        # errors raised
+        except Exception as e:
+            _set_status_text(
+                [f"Could not open selected file: {sel} : {e}"] * 2,
+                status_col=status_col
+            )
+            print("Could not open selected file:", e)
+            return None, None
+        
+
+def make_select_run(list_file, real_time_tag):
+    """Create the Select widget for RTA modes.
+
+    Modes:
+        - Real time: retrieve data from RTA stream to display.
+        - Reading mode: retrieve data from selected file to display.
+    
+    Parameters
+    ----------
+    list_file : array_like
+        List of files in the storage directory.
+    real_time_tag : string
+        Tag representing the real-time mode.
+        Stored in static.constants.json.
+
+    Returns
+    -------
+    out : Select
+        Bokeh Select widget to display for selection.
+
+    """
+
+    return Select(
+        title="Run selected:",
+        value=real_time_tag,
+        options=list(list_file) + [real_time_tag]
+    )
+
+
+def make_status_col(file):
+    """Create the column layout of the metadata depending on the mode.
+
+    Modes:
+        - Real time: the listened stream.
+        - Reading mode: the selected file.
+
+    Parameters
+    ----------
+    file : hdf5 file
+        File where data is stored.
+        **Need to add stream too.**
+
+    Returns
+    -------
+    out : column
+        Bokeh column layout of the file/stream metadata.
+
+    """
+
+    file_div = Div(text=f"Loaded file: {file.filename}")
+    time_div = Div(text=f"Last update: {time.strftime('%H:%M:%S')}")
+    return column(file_div, time_div)
+
+
+def make_header_menu(ressource_path, real_time_tag, file=None, extension=".h5"):
+    """Create full header menu.
+
+    Parameters
+    ----------
+    ressource_path : string
+        Path of the directory where to find the files to list.
+        Can be relative or absolute (careful if it is relative,
+        might be an issue for portability).
+    file : hdf5 file, optional
+        File of data to display.
+        Default is None, meaning last file of the storage directory is taken.
+    extension : string, optional
+        Extension of the format of files to be listed.
+        Default is .h5.
+
+    Returns
+    -------
+    out : tuple of (Select, column of Div)
+        Full header menu
+
+    """
+
+    if file is None:
+        file = _get_latest_file(ressource_path, extension=extension)
+    list_file = _list_runs(ressource_path, extension=extension)
+    run_choice_slidedown = make_select_run(list_file, real_time_tag)
+        
+    status_col = make_status_col(file)
+    return run_choice_slidedown, status_col
---
+++ src/nectarchain/RTA_dqm/utils/display/summary.py
@@ -0,0 +1,168 @@
+# Author: Julian Hamo
+# Contact mail: julian.hamo@ijclab.in2p3.fr
+
+"""
+This module stores the Bokeh webpage summary card maker for the RTA of NectarCAM.
+"""
+
+# imports
+import numpy as np
+
+# Bokeh imports
+from bokeh.models import Div
+from bokeh.layouts import column
+
+
+__all__ = ["make_summary_card", "update_summary_card"]
+
+
+def make_summary_card(file, display_registry, parentkeys, childkeys, run_index=-1):
+    """Make the summary card of the currently observed event.
+
+    Parameters
+    ----------
+    file : dict_like
+        Data of the considered run.
+    parentkeys : dict
+        Parent keys of the file to retrieve data.
+    childkeys : dict
+        Child keys of the file to retrieve data.
+    run_index : int, optional
+        A file is constituted of multiple events,
+        select an event in the stored file.
+        Default is -1, resulting in the latest event of the run.
+
+    Returns
+    -------
+    out : column
+        Bokeh layout column of the summary card of the seen event.
+
+    """
+
+    tel_id = file[parentkeys["parameter_parentkey"]][childkeys["tel_id"]][run_index]
+    event_id = file[parentkeys["parameter_parentkey"]][childkeys["event_id"]][run_index]
+    layout_id = np.where(file[parentkeys["layout_parentkey"]][childkeys["tel_id"]] == tel_id)[0][0]
+    trigger_id = np.where(file[parentkeys["trigger_parentkey"]][childkeys["event_id"]] == event_id)[0][0]
+    name = file[parentkeys["layout_parentkey"]][childkeys["name"]][layout_id].decode("utf-8")
+    camera_type = file[parentkeys["layout_parentkey"]][childkeys["camera_type"]][layout_id].decode("utf-8")
+    time = file[parentkeys["trigger_parentkey"]][childkeys["time"]][trigger_id]
+    timestamp_qns = file[parentkeys["trigger_parentkey"]][childkeys["timestamp_qns"]][trigger_id]
+    alt_tel = file[parentkeys["parameter_parentkey"]][childkeys["alt_tel"]][run_index]
+    az_tel = file[parentkeys["parameter_parentkey"]][childkeys["az_tel"]][run_index]
+    obs_id = file[parentkeys["parameter_parentkey"]][childkeys["obs_id"]][run_index]
+    event_type = file[parentkeys["parameter_parentkey"]][childkeys["event_type"]][run_index]
+    event_quality = file[parentkeys["parameter_parentkey"]][childkeys["event_quality"]][run_index]
+    event_goodness = file[parentkeys["parameter_parentkey"]][childkeys["is_good_event"]][run_index]
+    
+    title = Div(
+        text="<strong>Summary card:</strong>",
+    )
+    card = Div(
+        text=f"""
+            <div>
+              Telescope: {name} - id: {tel_id}<br>
+              Camera: {camera_type}<br>
+              Position:<br>
+              <dl>
+              <dd>altitude: {alt_tel}</dd>
+              <dd>azimut: {az_tel}</dd>
+              </dl>
+              Observation: {obs_id}<br>
+              Event:<br>
+              <dl>
+              <dd>event type: {event_type}</dd>
+              <dd>id: {event_id}</dd>
+              <dd>event quality: {event_quality}</dd>
+              <dd>event goodness: {event_goodness}</dd>
+              </dl>
+              Observation time: {time} [units]<br>
+              Timestamp: {timestamp_qns / 1e9} [s]<br>
+            </div>
+        """, 
+        styles={
+            "border": "2px solid #e2e8f0",
+            "padding": "12px",
+            "border-radius": "8px",
+            "width": "300px",
+        },
+        width=300, height=300
+    )
+
+    display = column(title, card)
+    display._meta = {
+        "type": "summary_card",
+        "parentkey": parentkeys,
+        "childkey": childkeys,
+        "factory": "make_summary_card"
+    }
+    display_registry.append(display)
+
+    return display
+
+
+def update_summary_card(disp, file, parentkeys, childkeys, run_index=-1):
+    """Make the summary card of the currently observed event.
+
+    Parameters
+    ----------
+    disp : column
+        Summary card to display in the shape of a Bokeh layout column.
+    file : dict_like
+        Data of the considered run.
+    parentkeys : dict
+        Parent keys of the file to retrieve data.
+    childkeys : dict
+        Child keys of the file to retrieve data.
+    run_index : int, optional
+        A file is constituted of multiple events,
+        select an event in the stored file.
+        Default is -1, resulting in the latest event of the run.
+
+    """
+
+    try:
+        if hasattr(disp, "children") and len(disp.children) > 1:
+            if hasattr(disp.children[1], "text"):
+                
+                tel_id = file[parentkeys["parameter_parentkey"]][childkeys["tel_id"]][run_index]
+                event_id = file[parentkeys["parameter_parentkey"]][childkeys["event_id"]][run_index]
+                layout_id = np.where(file[parentkeys["layout_parentkey"]][childkeys["tel_id"]] == tel_id)[0][0]
+                trigger_id = np.where(file[parentkeys["trigger_parentkey"]][childkeys["event_id"]] == event_id)[0][0]
+                name = file[parentkeys["layout_parentkey"]][childkeys["name"]][layout_id].decode("utf-8")
+                camera_type = file[parentkeys["layout_parentkey"]][childkeys["camera_type"]][layout_id].decode("utf-8")
+                time = file[parentkeys["trigger_parentkey"]][childkeys["time"]][trigger_id]
+                timestamp_qns = file[parentkeys["trigger_parentkey"]][childkeys["timestamp_qns"]][trigger_id]
+                alt_tel = file[parentkeys["parameter_parentkey"]][childkeys["alt_tel"]][run_index]
+                az_tel = file[parentkeys["parameter_parentkey"]][childkeys["az_tel"]][run_index]
+                obs_id = file[parentkeys["parameter_parentkey"]][childkeys["obs_id"]][run_index]
+                event_type = file[parentkeys["parameter_parentkey"]][childkeys["event_type"]][run_index]
+                event_quality = file[parentkeys["parameter_parentkey"]][childkeys["event_quality"]][run_index]
+                event_goodness = file[parentkeys["parameter_parentkey"]][childkeys["is_good_event"]][run_index]   
+
+                disp.children[1].text = f"""
+                    <div>
+                    Telescope: {name} - id: {tel_id}<br>
+                    Camera: {camera_type}<br>
+                    Position:<br>
+                    <dl>
+                    <dd>altitude: {alt_tel}</dd>
+                    <dd>azimut: {az_tel}</dd>
+                    </dl>
+                    Observation: {obs_id}<br>
+                    Event:<br>
+                    <dl>
+                    <dd>event type: {event_type}</dd>
+                    <dd>id: {event_id}</dd>
+                    <dd>event quality: {event_quality}</dd>
+                    <dd>event goodness: {event_goodness}</dd>
+                    </dl>
+                    Observation time: {time} [units]<br>
+                    Timestamp: {timestamp_qns / 1e9} [s]<br>
+                    </div>
+                """
+            else:
+                print("No .text attribute found.")
+        else:
+            print("No .children attribute found.")
+    except Exception as e:
+        print("Failed to update summary card:", e)
