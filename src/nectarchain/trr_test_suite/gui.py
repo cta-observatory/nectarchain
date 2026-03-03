f31d6c621b465039daeea6405a0f645a154937ad
Jean-Philippe Lenain <jlenain@in2p3.fr>
2026-03-03 14:39:07 +0100

    Small harmonization of camel cases for list of tests

--- src/nectarchain/trr_test_suite/gui.py
+++ src/nectarchain/trr_test_suite/gui.py
@@ -68,7 +68,7 @@ class TestRunner(QWidget):
         "Charge Resolution Test": charge_resolution,
         "Linearity Test": linearity,
         "Deadtime Test": deadtime,
-        "Flat-field source characterization test": flatfield_source_characterization,
+        "Flat-field Source Characterization Test": flatfield_source_characterization,
         "Pedestal Test": pedestal,
         "Pixel Time Uncertainty Test": pix_tim_uncertainty,
         "Time Uncertainty Between Couples of Pixels": pix_couple_tim_uncertainty,
