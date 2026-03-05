a158bf408360c3d241168125bf29eb59cdd3890a
medha1996c <chakrabortymedha907@gmail.com>
2026-03-05 18:05:11 +0100

    Adding Linearity submodule

--- src/nectarchain/trr_test_suite/observation_temperature.py
+++ src/nectarchain/trr_test_suite/observation_temperature.py
@@ -8,8 +8,10 @@ import numpy as np
 import pandas as pd
 from charge_resolution import run_charge_resolution
 from deadtime import run_deadtime
+from linearity import run_linearity
+from lmfit.models import Model
 
-# from linearity import run_linearity
+from nectarchain.trr_test_suite.utils import linear_fit_function
 
 logging.basicConfig(
     format="%(asctime)s %(name)s %(levelname)s %(message)s",
@@ -21,7 +23,8 @@ log = logging.getLogger(__name__)
 try:
     plt.style.use(
         os.path.join(
-            os.path.abspath(os.path.dirname(__file__)), "../utils/plot_style.mpltstyle"
+            os.path.abspath(os.path.dirname(__file__)),
+            "../utils/plot_style_new.mpltstyle",
         )
     )
 except FileNotFoundError as e:
@@ -135,7 +138,7 @@ class ObservationTemperaturePipeline:
             if module == "charge_resolution":
                 self.plot_charge_resolution_temperature(df_module_filtered)
             elif module == "linearity":
-                self.plot_linearity_temperature()
+                self.plot_linearity_temperature(df_module_filtered)
             elif module == "deadtime":
                 self.plot_deadtime_temperature(df_module_filtered)
             else:
@@ -289,8 +292,247 @@ class ObservationTemperaturePipeline:
         plt.legend(fontsize=8, ncol=2)
         plt.savefig("ChargeResolution_T_nsb_paper.png")
 
-    def plot_linearity_temperature():
-        pass
+    def plot_linearity_temperature(self, df_module_filtered):
+        """Plots
+        1. a, b coefficients with temperature
+        2. HG/LG with temperature
+        """
+
+        results = []
+
+        for _, row in df_module_filtered.iterrows():
+            t = row["temperature"]
+            runlist = row["runs"]
+            transmission = row["transmissions"]
+
+            print(row, "====", t, runlist, transmission)
+
+            fit_parameters, ratio, ratio_std = run_linearity(
+                runlist,
+                transmission,
+                temperature=t,
+                nevents=self.max_events,
+                output_dir=self.output_dir,
+            )
+
+            # unpack HG and LG
+            (a_HG, b_HG, a_err_HG, b_err_HG) = fit_parameters[0]
+            (a_LG, b_LG, a_err_LG, b_err_LG) = fit_parameters[1]
+
+            results.append(
+                {
+                    "temperature": t,
+                    "a_HG": a_HG,
+                    "b_HG": b_HG,
+                    "a_err_HG": a_err_HG,
+                    "b_err_HG": b_err_HG,
+                    "a_LG": a_LG,
+                    "b_LG": b_LG,
+                    "a_err_LG": a_err_LG,
+                    "b_err_LG": b_err_LG,
+                    "ratio": ratio,
+                    "ratio_std": ratio_std,
+                    "transmission": transmission,
+                }
+            )
+
+        df_lin = pd.DataFrame(results)
+        df_lin = df_lin.sort_values("temperature")  # sort values with temperature
+
+        # PLOT 1 : Parameters vs temperature
+        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(6, 6))
+
+        # Parameter a with temperature
+        ax1.errorbar(
+            df_lin["temperature"],
+            df_lin["a_HG"],
+            yerr=df_lin["a_err_HG"],
+            marker="o",
+            ls=" ",
+            label="$a_{HG}$",
+        )
+        model = Model(linear_fit_function)
+        params = model.make_params(a=10, b=0)
+        a_HG_fit = model.fit(
+            df_lin["a_HG"],
+            params,
+            weights=1 / df_lin["a_err_HG"],
+            x=df_lin["temperature"],
+        )
+        ax1.plot(
+            df_lin["temperature"],
+            linear_fit_function(
+                df_lin["temperature"],
+                a_HG_fit.params["a"].value,
+                a_HG_fit.params["b"].value,
+            ),
+            ls="--",
+            label=r" Fit line, slope: $(%2.4f\pm%2.4f)$ $^{\circ} C^{-1}$ "
+            % (
+                a_HG_fit.params["a"].value,
+                a_HG_fit.params["a"].stderr,
+            ),
+        )
+        ax1.errorbar(
+            df_lin["temperature"],
+            df_lin["a_LG"],
+            yerr=df_lin["a_err_LG"],
+            marker="s",
+            ls=" ",
+            label="$a_{LG}$",
+        )
+        model = Model(linear_fit_function)
+        params = model.make_params(a=10, b=0)
+        a_LG_fit = model.fit(
+            df_lin["a_LG"],
+            params,
+            weights=1 / df_lin["a_err_LG"],
+            x=df_lin["temperature"],
+        )
+        ax1.plot(
+            df_lin["temperature"],
+            linear_fit_function(
+                df_lin["temperature"],
+                a_LG_fit.params["a"].value,
+                a_LG_fit.params["b"].value,
+            ),
+            ls="--",
+            label=r" Fit line, slope: $(%2.4f\pm%2.4f)$ $^{\circ} C^{-1}$ "
+            % (
+                a_LG_fit.params["a"].value,
+                a_LG_fit.params["a"].stderr,
+            ),
+        )
+
+        ax1.legend(fontsize=8)
+        ax1.set_xlim(-7, 28)
+        # Parameter b vs temperature
+        ax2.errorbar(
+            df_lin["temperature"],
+            df_lin["b_HG"],
+            yerr=df_lin["b_err_HG"],
+            marker="o",
+            ls=" ",
+            label="$b_{HG}$",
+        )
+        model = Model(linear_fit_function)
+        params = model.make_params(a=10, b=0)
+        b_HG_fit = model.fit(
+            df_lin["b_HG"],
+            params,
+            weights=1 / df_lin["b_err_HG"],
+            x=df_lin["temperature"],
+        )
+        ax2.plot(
+            df_lin["temperature"],
+            linear_fit_function(
+                df_lin["temperature"],
+                b_HG_fit.params["a"].value,
+                b_HG_fit.params["b"].value,
+            ),
+            ls="--",
+            label=r" Fit line, slope: $(%2.3f\pm%2.3f)$ $^{\circ} p.e. C^{-1}$ "
+            % (
+                b_HG_fit.params["a"].value,
+                b_HG_fit.params["a"].stderr,
+            ),
+        )
+        ax2.errorbar(
+            df_lin["temperature"],
+            df_lin["b_LG"],
+            yerr=df_lin["b_err_LG"],
+            marker="s",
+            ls=" ",
+            label="$b_{LG}$",
+        )
+
+        model = Model(linear_fit_function)
+        params = model.make_params(a=10, b=0)
+        b_LG_fit = model.fit(
+            df_lin["b_LG"],
+            params,
+            weights=1 / df_lin["b_err_LG"],
+            x=df_lin["temperature"],
+        )
+        ax2.plot(
+            df_lin["temperature"],
+            linear_fit_function(
+                df_lin["temperature"],
+                b_LG_fit.params["a"].value,
+                b_LG_fit.params["b"].value,
+            ),
+            ls="--",
+            label=r" Fit line, slope: $(%2.3f\pm%2.3f)$ $^{\circ} p.e. C^{-1}$ "
+            % (
+                b_LG_fit.params["a"].value,
+                b_LG_fit.params["a"].stderr,
+            ),
+        )
+
+        ax2.set_xlabel("Temperature ($deg C$)")
+        ax2.legend(fontsize=8)
+        ax2.set_xlim(-7, 28)
+        plt.savefig("Linearity_params_temp.png")
+
+        # PLOT2 : HG/LG as function of different transmissions
+        slope_hglg = []
+        print("loop", df_lin)
+        df_lin = df_lin.explode(["transmission", "ratio", "ratio_std"])
+        plt.clf()
+        print("before loop ", sorted(df_lin["transmission"].unique()))
+        for transmission in sorted(df_lin["transmission"].unique()):
+            print(transmission)
+
+            df_lin_transmission = df_lin[
+                df_lin["transmission"] == transmission
+            ].sort_values("temperature")
+            print(df_lin_transmission)
+            plt.errorbar(
+                df_lin_transmission["temperature"],
+                df_lin_transmission["ratio"],
+                marker="s",
+                alpha=0.8,
+                label=f"Tr = {transmission}",
+            )
+
+            x = df_lin_transmission["temperature"].to_numpy(dtype=float)
+            y = df_lin_transmission["ratio"].to_numpy(dtype=float)
+            yerr = df_lin_transmission["ratio_std"].to_numpy(dtype=float)
+
+            y1 = y - yerr
+            y2 = y + yerr
+
+            plt.fill_between(x, y1, y2, alpha=0.3)
+
+            model = Model(linear_fit_function)
+            params = model.make_params(a=10, b=0)
+            ratio_fit = model.fit(
+                df_lin_transmission["ratio"],
+                params,
+                weights=1 / df_lin_transmission["ratio_std"],
+                x=df_lin_transmission["temperature"],
+            )
+            slope_hglg.append(ratio_fit.params["a"].value)
+        slope_hglg = np.array(slope_hglg)
+        plt.text(
+            0.05,
+            0.98,
+            r"Avg slope : $(%2.3f\pm%2.3f)$"
+            % (np.mean(slope_hglg), np.std(slope_hglg) / np.sqrt(len(slope_hglg))),
+            transform=plt.gca().transAxes,
+            ha="left",
+            va="top",
+            fontsize=6,
+            bbox=dict(
+                boxstyle="round", facecolor="white", edgecolor="black", alpha=0.9
+            ),
+        )
+        plt.xlabel("Temperature $deg C")
+        plt.ylabel("HG/LG")
+        plt.legend(fontsize=5, ncol=3)
+        plt.xlim(-10, 30)
+        plt.ylim(-1, 20)
+        plt.savefig("HGLGRatio_Linearity_temp.png")
 
     def plot_deadtime_temperature(self, df_module_filtered):
         """Plots
