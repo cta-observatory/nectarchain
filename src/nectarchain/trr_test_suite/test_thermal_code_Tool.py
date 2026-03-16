e4739c54b87ce0969c775948b1e30c2aa012c6b6
Amélie CLÉMENT <aclement@cca017.in2p3.fr>
2026-03-16 16:24:16 +0100

    tools_components compatible temperary update

--- src/nectarchain/trr_test_suite/test_thermal_code_Tool.py
+++ src/nectarchain/trr_test_suite/test_thermal_code_Tool.py
@@ -387,8 +387,7 @@ def run_long_run_test_tool(
     tool.start()
     output = tool.finish(gain_file=gain_file_name, id=ids)
     # output = read_file(run, temperature)
-    (
-        mean_charge_all,
+    (    mean_charge_all,
         std_charge_all,
         std_err_all,
         npixels,
@@ -407,6 +406,8 @@ def run_long_run_test_tool(
         time_tot_all,
         deadtime_pc_all,
     ) = output
+
+    print(f"{output=}")
     # ucts_timestamps = output[0]
     """ucts_deltat = output[1]
 
