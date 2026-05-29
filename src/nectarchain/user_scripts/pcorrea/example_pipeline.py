7168b9b7c78c557e465c613268415e785de6c2d3
Pablo Correa <38924085+pcorcam@users.noreply.github.com>
2026-05-29 11:05:09 +0200

    Clean up configuration usage and add HiLo tool

--- src/nectarchain/user_scripts/pcorrea/example_pipeline.py
+++ src/nectarchain/user_scripts/pcorrea/example_pipeline.py
@@ -4,9 +4,20 @@ import os
 import numpy as np
 from traitlets.config import Config
 
+from nectarchain.makers.calibration import (
+    FlatfieldNectarCAMCalibrationTool,
+    FlatFieldSPECombinedStdNectarCAMCalibrationTool,
+    FlatFieldSPEHHVNectarCAMCalibrationTool,
+    FlatFieldSPEHHVStdNectarCAMCalibrationTool,
+    FlatFieldSPENominalNectarCAMCalibrationTool,
+    FlatFieldSPENominalStdNectarCAMCalibrationTool,
+    HiLoNectarCAMCalibrationTool,
+    PedestalNectarCAMCalibrationTool,
+)
 from nectarchain.makers.calibration.calibration_pipeline import (
     PipelineNectarCAMCalibrationTool,
 )
+from nectarchain.makers.calibration.core import NectarCAMCalibrationTool
 
 logging.basicConfig(
     format="%(asctime)s %(name)s %(levelname)s %(message)s", level=logging.INFO
@@ -14,16 +25,22 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 log.handlers = logging.getLogger("__main__").handlers
 
-# Some the run numbers to use for each calibration tool
+
+######################
+# Tool configuration #
+######################
+
+# Run numbers to use for each calibration tool
 ped_run_number = 6249
 FF_run_number = 6252
 FF_SPE_run_number = 3936
 FF_SPE_HHV_run_number = 3942
 
-# Specify which tools to use for each step in the calibration
-ped_tool_name = "PedestalNectarCAMCalibrationTool"
-gain_tool_name = "FlatFieldSPENominalStdNectarCAMCalibrationTool"
-FF_tool_name = "FlatfieldNectarCAMCalibrationTool"
+# Tools to use for each step in the calibration
+ped_tool_name = PedestalNectarCAMCalibrationTool.__name__
+gain_tool_name = FlatFieldSPENominalStdNectarCAMCalibrationTool.__name__
+hilo_tool_name = HiLoNectarCAMCalibrationTool.__name__
+FF_tool_name = FlatfieldNectarCAMCalibrationTool.__name__
 
 # Path for a 1400-V result file of the SPE fit
 # used in the SPE combined fit and photostatistic method
@@ -33,19 +50,40 @@ SPE_HHV_result_path = (
     "window_shift_4_window_width_8.h5"
 )
 
-# Some general configurations that will pass to all subtools
-max_events = 12000
-progress_bar = True
-overwrite = True
-
-# Some specific configurations for each subtool
 config = Config()
-config[ped_tool_name].events_per_slice = 5000
+
+# Global configurations for all tools
+core_tool_name = NectarCAMCalibrationTool.__name__
+config[core_tool_name].max_events = 10000
+config[core_tool_name].progress_bar = True
+config[core_tool_name].overwrite = True
+config[core_tool_name].log_level = "INFO"
+config[core_tool_name].camera = "NectarCAMQM"
+
+# Configure pedestal tool
+config[ped_tool_name].events_per_slice = 3000
+config[ped_tool_name].method = "FullWaveformSum"
+
+# Configure gain tool
+config[gain_tool_name].method = "LocalPeakWindowSum"
+config[gain_tool_name].extractor_kwargs = {"window_width": 8, "window_shift": 4}
 config[gain_tool_name].multiproc = True
 config[gain_tool_name].nproc = 8
-# config[gain_tool_name].display = False
-asked_pixels_id = np.arange(100)
-config[gain_tool_name].asked_pixels_id = asked_pixels_id.tolist()
+config[gain_tool_name].asked_pixels_id = np.arange(100).tolist()
+
+# Configure HiLo tool
+config[hilo_tool_name].method = config[gain_tool_name].method
+config[hilo_tool_name].extractor_kwargs = config[gain_tool_name].extractor_kwargs
+
+# Configure flat-field tool
+config[FF_tool_name].charge_extraction_method = "LocalPeakWindowSum"
+config[FF_tool_name].window_width = 12
+config[FF_tool_name].window_shift = 4
+
+
+#################
+# Main function #
+#################
 
 
 def main():
@@ -58,10 +96,8 @@ def main():
         SPE_HHV_result_path=SPE_HHV_result_path,
         ped_tool_name=ped_tool_name,
         gain_tool_name=gain_tool_name,
+        hilo_tool_name=hilo_tool_name,
         FF_tool_name=FF_tool_name,
-        max_events=max_events,
-        progress_bar=progress_bar,
-        overwrite=overwrite,
     )
 
     tool.run()
