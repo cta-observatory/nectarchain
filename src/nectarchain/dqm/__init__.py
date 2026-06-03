ca65bb9ad37ec9146d3dbb32fc0c550351054a2d
Alessandro Montanari <49238256+alessandromontanari@users.noreply.github.com>
2026-06-03 16:42:55 +0200

    feat(src/nectarchain/dqm): (#316)
    - __init__.py: change processor init name for waveforms
    
    - waveforms.py: moved from mean_waveforms.py, include all waveforms (not only one averaged over all pixels) in the dictionary saved to database
    
    - start_dqm.py: minor fixes following changes in waveforms processor script
    
    - tests/test_waveforms.py

--- src/nectarchain/dqm/__init__.py
+++ src/nectarchain/dqm/__init__.py
@@ -6,17 +6,17 @@ from .camera_monitoring import CameraMonitoring
 from .charge_integration import ChargeIntegrationHighLowGain
 from .dqm_summary_processor import DQMSummary
 from .mean_camera_display import MeanCameraDisplayHighLowGain
-from .mean_waveforms import MeanWaveFormsHighLowGain
 from .pixel_participation import PixelParticipationHighLowGain
 from .pixel_timeline import PixelTimelineHighLowGain
 from .trigger_statistics import TriggerStatistics
+from .waveforms import WaveFormsHighLowGain
 
 __all__ = [
     "CameraMonitoring",
     "ChargeIntegrationHighLowGain",
     "DQMSummary",
     "MeanCameraDisplayHighLowGain",
-    "MeanWaveFormsHighLowGain",
+    "WaveFormsHighLowGain",
     "PixelParticipationHighLowGain",
     "PixelTimelineHighLowGain",
     "TriggerStatistics",
