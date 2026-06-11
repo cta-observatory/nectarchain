c46c3f3227e061a7ccc958c3eff4f494b6f0c5c6
Amélie CLÉMENT <aclement@cca017.in2p3.fr>
2026-06-11 17:08:02 +0200

    pedestal with respect to time

--- src/nectarchain/acceptance_verification_package/gui.py
+++ src/nectarchain/acceptance_verification_package/gui.py
@@ -2,7 +2,10 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from nectarchain.acceptance_verification_package import hillas_validation
+from nectarchain.acceptance_verification_package import (
+    hillas_validation,
+    ped_vs_time,
+)
 from nectarchain.trr_test_suite import deadtime
 from nectarchain.trr_test_suite.gui import TestRunner
 
@@ -11,6 +14,7 @@ class AcceptanceTestRunner(TestRunner):
     # redefine list of test modules
     test_modules = {
         "Hillas parameter validation": hillas_validation,
+        "Pedestal evolution in time": ped_vs_time,
         "Deadtime Test": deadtime,
     }
 
