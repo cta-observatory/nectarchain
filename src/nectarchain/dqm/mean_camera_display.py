c9cd8abc87dfa38a9c0a6332f48f7c8697097999
alessandromontanari <a.montanari.05@gmail.com>
2026-08-26 17:08:38 +0200

    perf(src/nectarchain/dqm): - mean_camera_display.py: general perf optimisation for whole processor

--- src/nectarchain/dqm/mean_camera_display.py
+++ src/nectarchain/dqm/mean_camera_display.py
@@ -17,27 +17,38 @@ class MeanCameraDisplayHighLowGain(DQMSummary):
         self.Pix = None
         self.Samp = None
         self.tel_id = None
-        self.CameraAverage = None
-        self.CameraAverage_ped = None
         self.counter_evt = None
         self.counter_ped = None
         self.camera = None
         self.cmap = None
-        self.CameraAverage = []
-        self.CameraAverage1 = []
-        self.CameraAverage_ped = []
-        self.CameraAverage_ped1 = []
-        self.CameraAverage_overEvents = None
-        self.CameraAverage_overEvents_overSamp = None
-        self.CameraAverage_ped_overEvents = None
-        self.CameraAverage_ped_overEvents_overSamp = None
+        self.camera_average = None
+        self.camera_average_ped = None
+        self.camera_average_over_events = None
+        self.camera_average_over_events_over_samp = None
+        self.camera_average_ped_over_events = None
+        self.camera_average_ped_over_events_over_samp = None
+        self.pixel_bad = None
+        self.pixel_ids = None
         self.MeanCameraDisplay_Results_Dict = {}
         self.MeanCameraDisplay_Figures_Dict = {}
         self.MeanCameraDisplay_Figures_Names_Dict = {}
+
+        gain_c = "High" if self.k == 0 else "Low"
+        self.gain_c = gain_c
+
+        self.figure_keys = {
+            "physical": f"CAMERA-AVERAGE-PHY-DISPLAY-{gain_c}-GAIN",
+            "pedestals": f"CAMERA-AVERAGE-PED-DISPLAY-{gain_c}-GAIN",
+        }
+
+        self.figure_filenames = {
+            "physical": f"_Camera_Mean_{gain_c}Gain.png",
+            "pedestals": f"_Pedestal_Mean_{gain_c}Gain.png",
+        }
+
         super().__init__(r0)
 
     def configure_for_run(self, path, Pix, Samp, Reader1, **kwargs):
-        # define number of pixels and samples
         self.Pix = Pix
         self.Samp = Samp
 
@@ -52,149 +63,153 @@ class MeanCameraDisplayHighLowGain(DQMSummary):
 
         self.cmap = "gnuplot2"
 
-    def process_event(self, evt, noped):
-        self.pixelBAD = evt.mon.tel[self.tel_id].pixel_status.hardware_failing_pixels
-        pixel = evt.nectarcam.tel[self.tel_id].svc.pixel_ids
-        if len(pixel) < self.Pix:
-            pixel21 = list(np.arange(0, self.Pix - len(pixel), 1, dtype=int))
-            pixel = list(pixel)
-            pixels = np.concatenate([pixel21, pixel])
-        else:
-            pixels = pixel
+        # Pre-allocate accumulation arrays with float32 for memory efficiency
+        self.camera_average = np.zeros(Pix, dtype=np.float32)
+        self.camera_average_ped = np.zeros(Pix, dtype=np.float32)
 
-        if self.r0:
-            waveforms = evt.r0.tel[self.tel_id].waveform[self.k]
+        # Initialize pixel mapping - will be set from first event
+        self.pixel_bad = None
+        self.pixel_ids = None
+
+    def process_event(self, evt, noped):
+        # Extract pixel information from event
+        pixel_bad = evt.mon.tel[self.tel_id].pixel_status.hardware_failing_pixels
+        pixel_ids = evt.nectarcam.tel[self.tel_id].svc.pixel_ids
+
+        # Store pixel bad mask from first event (assumed constant per run)
+        if self.pixel_bad is None:
+            self.pixel_bad = pixel_bad
+
+        # Handle pixel mapping - create full array if needed
+        if len(pixel_ids) < self.Pix:
+            padding = np.arange(0, self.Pix - len(pixel_ids), 1, dtype=int)
+            pixels = np.concatenate([padding, pixel_ids])
         else:
+            pixels = pixel_ids
+
+        # Extract waveforms based on r0/r1 mode
+        if self.r0:
             # This should accommodate cases were the shape of waveforms is 2D
             # (1855,60), or 3D (2, 1855, 60) for 2-gain channels or
             # (1, 1855, 60) for single-gain channel
-            waveforms = evt.r1.tel[self.tel_id].waveform
-
-        if evt.trigger.event_type == EventType.SKY_PEDESTAL:
-            # count sky peds, event id 2
-            self.counter_ped += 1
-            self.CameraAverage_ped1 = waveforms.sum(axis=-1)
-            self.CameraAverage_ped.append(self.CameraAverage_ped1[pixels])
-        elif evt.trigger.event_type == EventType.SUBARRAY:
-            # count standard physics stereo events, event id 32
-            self.counter_evt += 1
-            self.CameraAverage1 = waveforms.sum(axis=-1)
-            self.CameraAverage.append(self.CameraAverage1[pixels])
-        # TODO: add ids for other event types, e.g., dark pedestals
-        # TODO: this else is wrong, we should have a separate counter
-        # for other event types, e.g., dark pedestals. It has to be implemented.
+            waveforms = evt.r0.tel[self.tel_id].waveform[self.k]
         else:
+            wf = evt.r1.tel[self.tel_id].waveform
+            if wf.ndim == 3:
+                waveforms = wf[self.k]
+            else:
+                waveforms = wf
+
+        # Detect event type with boolean flags (following waveforms.py pattern)
+        is_ped = evt.trigger.event_type == EventType.SKY_PEDESTAL
+        is_phy = evt.trigger.event_type == EventType.SUBARRAY
+
+        # Sum waveforms over samples (axis=-1) to get charge per pixel
+        charge_per_pixel = waveforms.sum(axis=-1)
+
+        # Reindex using pixel mapping and accumulate directly into pre-allocated arrays
+        charge_mapped = charge_per_pixel[pixels]
+
+        if is_ped:
+            self.counter_ped += 1
+            self.camera_average_ped += charge_mapped
+        elif is_phy:
             self.counter_evt += 1
-            self.CameraAverage1 = waveforms.sum(axis=-1)
-            self.CameraAverage.append(self.CameraAverage1[pixels])
+            self.camera_average += charge_mapped
+        else:
+            # TODO: add ids for other event types, e.g., dark pedestals
+            # TODO: this else is wrong, we should have a separate counter
+            # for other event types, e.g., dark pedestals. It has to be implemented.
+            self.counter_evt += 1
+            self.camera_average += charge_mapped
 
         return None
 
     def finish_run(self):
         if self.counter_evt > 0:
-            self.CameraAverage = np.array(self.CameraAverage)
-            self.CameraAverage = self.CameraAverage.sum(axis=0)
-            self.CameraAverage_overEvents = self.CameraAverage / self.counter_evt
-
-            self.CameraAverage_overEvents_overSamp = (
-                self.CameraAverage_overEvents / self.Samp
+            # Compute mean over events
+            self.camera_average_over_events = self.camera_average / self.counter_evt
+            # Normalize by number of samples
+            self.camera_average_over_events_over_samp = (
+                self.camera_average_over_events / self.Samp
             )
 
         if self.counter_ped > 0:
-            self.CameraAverage_ped = np.array(self.CameraAverage_ped)
-            self.CameraAverage_ped = self.CameraAverage_ped.sum(axis=0)
-            self.CameraAverage_ped_overEvents = (
-                self.CameraAverage_ped / self.counter_ped
+            # Compute mean pedestals over events
+            self.camera_average_ped_over_events = (
+                self.camera_average_ped / self.counter_ped
             )
-            self.CameraAverage_ped_overEvents_overSamp = (
-                self.CameraAverage_ped_overEvents / self.Samp
+            # Normalize by number of samples
+            self.camera_average_ped_over_events_over_samp = (
+                self.camera_average_ped_over_events / self.Samp
             )
 
     def get_results(self):
-        # ASSIGN RESUTLS TO DICT
-        if self.k == 0:
-            if self.counter_evt > 0:
-                # self.MeanCameraDisplay_Results_Dict[
-                # "CAMERA-AVERAGE-OverEVENTS-HIGH-GAIN"
-                # ]  = self.CameraAverage_overEvents
-                self.MeanCameraDisplay_Results_Dict[
-                    "CAMERA-AVERAGE-PHY-OverEVENTS-OverSamp-HIGH-GAIN"
-                ] = self.CameraAverage_overEvents_overSamp
+        if self.counter_evt > 0:
+            # Handle NaN/Inf values (following waveforms.py pattern)
+            valid_mask = ~(
+                np.isinf(self.camera_average_over_events_over_samp)
+                | np.isnan(self.camera_average_over_events_over_samp)
+            )
+            if not np.all(valid_mask):
+                self.camera_average_over_events_over_samp[~valid_mask] = 0
 
-            if self.counter_ped > 0:
-                # self.MeanCameraDisplay_Results_Dict[
-                # "CAMERA-AVERAGE-PED-OverEVENTS-HIGH-GAIN"
-                # ]= self.CameraAverage_ped_overEvents
-                self.MeanCameraDisplay_Results_Dict[
-                    "CAMERA-AVERAGE-PED-OverEVENTS-OverSamp-HIGH-GAIN"
-                ] = self.CameraAverage_ped_overEvents_overSamp
+            self.MeanCameraDisplay_Results_Dict[
+                f"CAMERA-AVERAGE-PHY-OverEVENTS-OverSamp-{self.gain_c.upper()}-GAIN"
+            ] = self.camera_average_over_events_over_samp
 
-        if self.k == 1:
-            if self.counter_evt > 0:
-                # self.MeanCameraDisplay_Results_Dict[
-                # "CAMERA-AVERAGE-OverEVENTS-LOW-GAIN"
-                # ]  = self.CameraAverage_overEvents
-                self.MeanCameraDisplay_Results_Dict[
-                    "CAMERA-AVERAGE-PHY-OverEVENTS-OverSamp-LOW-GAIN"
-                ] = self.CameraAverage_overEvents_overSamp
+        if self.counter_ped > 0:
+            # Handle NaN/Inf values for pedestal data
+            valid_mask_ped = ~(
+                np.isinf(self.camera_average_ped_over_events_over_samp)
+                | np.isnan(self.camera_average_ped_over_events_over_samp)
+            )
+            if not np.all(valid_mask_ped):
+                self.camera_average_ped_over_events_over_samp[~valid_mask_ped] = 0
 
-            if self.counter_ped > 0:
-                # self.MeanCameraDisplay_Results_Dict[
-                # "CAMERA-AVERAGE-PED-OverEVENTS-LOW-GAIN"
-                # ]= self.CameraAverage_ped_overEvents
-                self.MeanCameraDisplay_Results_Dict[
-                    "CAMERA-AVERAGE-PED-OverEVENTS-OverSamp-LOW-GAIN"
-                ] = self.CameraAverage_ped_overEvents_overSamp
+            self.MeanCameraDisplay_Results_Dict[
+                f"CAMERA-AVERAGE-PED-OverEVENTS-OverSamp-{self.gain_c.upper()}-GAIN"
+            ] = self.camera_average_ped_over_events_over_samp
 
         return self.MeanCameraDisplay_Results_Dict
 
     def plot_results(self, name, fig_path):
-        # titles = ['All', 'Pedestals']
-        if self.k == 0:
-            gain_c = "High"
-        if self.k == 1:
-            gain_c = "Low"
-
         if self.counter_evt > 0:
-            fig1, disp1 = plt.subplots()
-            disp1 = CameraDisplay(
-                geometry=self.camera[~self.pixelBAD[0]],
-                image=self.CameraAverage_overEvents_overSamp[~self.pixelBAD[0]],
+            fig_key = self.figure_keys["physical"]
+            full_name = name + self.figure_filenames["physical"]
+            fig_path_full = os.path.join(fig_path, full_name)
+
+            fig, disp = plt.subplots()
+            disp = CameraDisplay(
+                geometry=self.camera[~self.pixel_bad[0]],
+                image=self.camera_average_over_events_over_samp[~self.pixel_bad[0]],
                 cmap=plt.cm.coolwarm,
             )
-            disp1.add_colorbar()
-            disp1.axes.text(2.0, 0, "Charge (DC)", rotation=90)
-            plt.title("Camera average %s gain (ALL)" % gain_c)
+            disp.add_colorbar()
+            disp.axes.text(2.0, 0, "Charge (DC)", rotation=90)
+            plt.title(f"Camera average {self.gain_c} gain (ALL)")
 
-            self.MeanCameraDisplay_Figures_Dict[
-                "CAMERA-AVERAGE-PHY-DISPLAY-%s-GAIN" % gain_c
-            ] = fig1
-            full_name = name + "_Camera_Mean_%sGain.png" % gain_c
-            FullPath = os.path.join(fig_path, full_name)
-            self.MeanCameraDisplay_Figures_Names_Dict[
-                "CAMERA-AVERAGE-PHY-DISPLAY-%s-GAIN" % gain_c
-            ] = FullPath
+            self.MeanCameraDisplay_Figures_Dict[fig_key] = fig
+            self.MeanCameraDisplay_Figures_Names_Dict[fig_key] = fig_path_full
             plt.close()
 
         if self.counter_ped > 0:
-            fig2, disp2 = plt.subplots()
-            disp2 = CameraDisplay(
-                geometry=self.camera[~self.pixelBAD[0]],
-                image=self.CameraAverage_ped_overEvents_overSamp[~self.pixelBAD[0]],
+            fig_key = self.figure_keys["pedestals"]
+            full_name = name + self.figure_filenames["pedestals"]
+            fig_path_full = os.path.join(fig_path, full_name)
+
+            fig, disp = plt.subplots()
+            disp = CameraDisplay(
+                geometry=self.camera[~self.pixel_bad[0]],
+                image=self.camera_average_ped_over_events_over_samp[~self.pixel_bad[0]],
                 cmap=plt.cm.coolwarm,
             )
-            disp2.add_colorbar()
-            disp2.axes.text(2.0, 0, "Charge (DC)", rotation=90)
-            plt.title("Camera average %s gain (PED)" % gain_c)
+            disp.add_colorbar()
+            disp.axes.text(2.0, 0, "Charge (DC)", rotation=90)
+            plt.title(f"Camera average {self.gain_c} gain (PED)")
 
-            self.MeanCameraDisplay_Figures_Dict[
-                "CAMERA-AVERAGE-PED-DISPLAY-%s-GAIN" % gain_c
-            ] = fig2
-            full_name = name + "_Pedestal_Mean_%sGain.png" % gain_c
-            FullPath = os.path.join(fig_path, full_name)
-            self.MeanCameraDisplay_Figures_Names_Dict[
-                "CAMERA-AVERAGE-PED-DISPLAY-%s-GAIN" % gain_c
-            ] = FullPath
+            self.MeanCameraDisplay_Figures_Dict[fig_key] = fig
+            self.MeanCameraDisplay_Figures_Names_Dict[fig_key] = fig_path_full
             plt.close()
 
         return (
