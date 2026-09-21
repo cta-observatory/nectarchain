ccf971ec39c0284b355c9ebc0f0bd16c73811290
Jean-Philippe Lenain <jlenain@in2p3.fr>
2026-09-21 14:57:21 +0200

    simplify call to class

--- src/nectarchain/dqm/bokeh_app/manual_test_mongo.py
+++ src/nectarchain/dqm/bokeh_app/manual_test_mongo.py
@@ -1,13 +1,20 @@
+"""Manual test: embed MongoExplorer as one tab in a multi-tab layout.
+
+Run with:
+    bokeh serve manual_test_mongo.py --show
+or from the bokeh_app directory:
+    python manual_test_mongo.py
+"""
+
 from bokeh.io import curdoc
 from bokeh.models import Div, TabPanel, Tabs
 from mongodb_explorer import MongoExplorer
 
-# Set the MongoDB connection details for testing
-MongoURL = "mongodb://192.168.30.104:27017"
-collection = "test"
-database = "runconfig"
+# MongoExplorer uses defaults from env vars (MONGO_URI, MONGO_DB, MONGO_COLLECTION)
+# or falls back to the hardcoded defaults in mongodb_explorer.py.
+# Pass explicit arguments to override.
+explorer = MongoExplorer()
 
-explorer = MongoExplorer(MongoURL, collection, database)
 another_tab = TabPanel(title="Another Tab", child=Div(text="Here will be dqm tabs"))
 tabs = Tabs(tabs=[another_tab, explorer.panel], sizing_mode="stretch_both")
 curdoc().add_root(tabs)
